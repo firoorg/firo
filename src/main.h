fddeefc26efbcea42d81ecd117d22a95cd83857e
nopslide <poramin.insom@gmail.com>
2016-09-28 22:28:52 -0700

    back maturity to 100

--- src/main.h
+++ src/main.h
@@ -61,7 +61,7 @@ static const int64 DUST_HARD_LIMIT = 1000;   // 0.00001 XZC mininput
 static const int64 MAX_MONEY = 21000000 * COIN;
 inline bool MoneyRange(int64 nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 /** Coinbase transaction outputs can only be spent after this number of new blocks (network rule) */
-static const int COINBASE_MATURITY = 1;
+static const int COINBASE_MATURITY = 100;
 /** Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp. */
 static const unsigned int LOCKTIME_THRESHOLD = 500000000; // Tue Nov  5 00:53:20 1985 UTC
 /** Maximum number of script-checking threads allowed */
