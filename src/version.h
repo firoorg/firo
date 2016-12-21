9eab893fc828ebd9aeb6709eedfa930da781db68
nopslide <poramin.insom@gmail.com>
2016-12-21 11:21:16 -0800

    update fouder reward

--- src/version.h
+++ src/version.h
@@ -25,7 +25,7 @@ extern const std::string CLIENT_DATE;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 90015;
+static const int PROTOCOL_VERSION = 90016;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 90013;
