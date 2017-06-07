b11224f1f30834d4ebd2b0937b2d94110243aa0f
Leandro Reinaux Almeida <leandro@even3.com.br>
2017-06-07 04:08:33 -0300

    setFormat test for build, line back

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -609,7 +609,7 @@ void BitcoinGUI::setNumBlocks(int count, int nTotalBlocks)
         }
 
         progressBarLabel->setVisible(true);
-        //progressBar->setFormat(tr("%1 behind").arg(timeBehindText));
+        progressBar->setFormat(tr("%1 behind").arg(timeBehindText));
         progressBar->setMaximum(1000000000);
         progressBar->setValue(clientModel->getVerificationProgress() * 1000000000.0 + 0.5);
         progressBar->setVisible(true);
