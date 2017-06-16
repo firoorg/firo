84d4067ed821e76cdb8e63e6e6789f4dee1766a3
Leandro Reinaux <leoreinaux@gmail.com>
2017-06-16 00:24:32 -0300

    fix datatypes

--- src/rpcwallet.cpp
+++ src/rpcwallet.cpp
@@ -398,7 +398,7 @@ Value verifymessage(const Array& params, bool fHelp)
     if (fInvalid)
         throw JSONRPCError(RPC_INVALID_ADDRESS_OR_KEY, "Malformed base64 encoding");
 
-    CHashWriter ss(SER_GETHASH, 0);
+    CDataStream ss(SER_GETHASH, 0);
     ss << strMessageMagic;
     ss << strMessage;
 
