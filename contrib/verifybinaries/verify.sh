9c867f9ba039cfb007633a56cd5a56fd2ad85449
snguyen.ntu@gmail.com <snguyen.ntu@gmail.com>
2017-09-18 22:43:05 +0700

    Fix broken gitian-build for OSX

--- contrib/verifybinaries/verify.sh
+++ contrib/verifybinaries/verify.sh
@@ -17,13 +17,13 @@ function clean_up {
    done
 }
 
-WORKINGDIR="/tmp/bitcoin"
+WORKINGDIR="/tmp/zcoin"
 TMPFILE="hashes.tmp"
 
 SIGNATUREFILENAME="SHA256SUMS.asc"
 RCSUBDIR="test/"
 BASEDIR="https://bitcoin.org/bin/"
-VERSIONPREFIX="bitcoin-core-"
+VERSIONPREFIX="zcoin-core-"
 RCVERSIONSTRING="rc"
 
 if [ ! -d "$WORKINGDIR" ]; then
