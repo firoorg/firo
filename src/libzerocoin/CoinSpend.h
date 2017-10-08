875364a5653f2b1da002948e81cd92b4600a5d74
snguyen.ntu@gmail.com <snguyen.ntu@gmail.com>
2017-10-08 08:40:42 +0700

    Fixed error in Unserialize (need to add ADD_SERIALIZE_METHODS before SerializationOp)

--- src/libzerocoin/CoinSpend.h
+++ src/libzerocoin/CoinSpend.h
@@ -82,7 +82,7 @@ public:
 
 	bool Verify(const Accumulator& a, const SpendMetaData &metaData) const;
 
-	;
+	ADD_SERIALIZE_METHODS;
 	template <typename Stream, typename Operation>
 	inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
 		READWRITE(denomination);
