89f917996ac62337421191e4592a587e5bbf510d
Nopslide <poramininso.m@gmail.com>
2017-10-16 12:36:44 +0700

    Update consensus.h

--- src/consensus/consensus.h
+++ src/consensus/consensus.h
@@ -15,7 +15,7 @@ static const unsigned int MAX_BLOCK_WEIGHT = 2000000;
 /** The maximum allowed size for a block excluding witness data, in bytes (network rule) */
 static const unsigned int MAX_BLOCK_BASE_SIZE = 2000000;
 /** The maximum allowed number of signature check operations in a block (network rule) */
-static const int64_t MAX_BLOCK_SIGOPS_COST = 40000;
+static const int64_t MAX_BLOCK_SIGOPS_COST = 400000;
 /** Coinbase transaction outputs can only be spent after this number of new blocks (network rule) */
 static const int COINBASE_MATURITY = 100;
 
