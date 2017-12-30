58dc2c2557c868fdc2ff8ddd0bfe705acfcd20d6
Renato Cruz <cruzrenato@gmail.com>
2017-12-29 19:18:08 -0800

    smartrewards build fix

--- src/qt/walletview.cpp
+++ src/qt/walletview.cpp
@@ -114,7 +114,7 @@ void WalletView::setClientModel(ClientModel *clientModel)
     overviewPage->setClientModel(clientModel);
     sendCoinsPage->setClientModel(clientModel);
     smartnodeListPage->setClientModel(clientModel);
-    smartrewardsListPage->setClientModel(clientModel);
+    //smartrewardsListPage->setClientModel(clientModel);
 }
 
 void WalletView::setWalletModel(WalletModel *walletModel)
@@ -130,7 +130,7 @@ void WalletView::setWalletModel(WalletModel *walletModel)
     usedReceivingAddressesPage->setModel(walletModel->getAddressTableModel());
     usedSendingAddressesPage->setModel(walletModel->getAddressTableModel());
     smartnodeListPage->setWalletModel(walletModel);
-    smartrewardsListPage->setWalletModel(walletModel);
+    smartrewardsListPage->setModel(walletModel);
 
     if (walletModel)
     {
