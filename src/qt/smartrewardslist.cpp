42552773c8c92adc5864318e58ac7136f1631410
Renato Cruz <cruzrenato@gmail.com>
2017-12-31 04:21:39 -0300

    Fix Smartrewards minimun amount and check snapshot date

--- src/qt/smartrewardslist.cpp
+++ src/qt/smartrewardslist.cpp
@@ -12,7 +12,6 @@
 #include "platformstyle.h"
 #include "txmempool.h"
 #include "walletmodel.h"
- 
 #include "coincontrol.h"
 #include "init.h"
 #include "main.h" // For minRelayTxFee
@@ -25,6 +24,18 @@
 #include <QMessageBox>
 #include <QSortFilterProxyModel>
 #include <QTableWidget>
+#include <QTime>
+
+int GetTimeOffsetFromUtc()
+{
+#if QT_VERSION < 0x050200
+    const QDateTime dateTime1 = QDateTime::currentDateTime();
+    const QDateTime dateTime2 = QDateTime(dateTime1.date(), dateTime1.time(), Qt::UTC);
+    return dateTime1.secsTo(dateTime2);
+#else
+    return QDateTime::currentDateTime().offsetFromUtc();
+#endif
+}
 
 SmartrewardsList::SmartrewardsList(const PlatformStyle *platformStyle, QWidget *parent) :
     QWidget(parent),
@@ -56,7 +67,7 @@ SmartrewardsList::~SmartrewardsList()
     if(!model) {
        return;
     }
- 
+
     ui->tableWidget->setAlternatingRowColors(true);
  
     int nDisplayUnit = model->getOptionsModel()->getDisplayUnit();
@@ -64,7 +75,6 @@ SmartrewardsList::~SmartrewardsList()
     std::map<QString, std::vector<COutput> > mapCoins;
     model->listCoins(mapCoins);
  
-    //ui->tableWidget->setRowCount(10);
     ui->tableWidget->setColumnCount(4);
     ui->tableWidget->setShowGrid(false);
  
@@ -79,27 +89,57 @@ SmartrewardsList::~SmartrewardsList()
         ui->tableWidget->insertRow(nNewRow);
  
         CAmount nSum = 0;
-        //double dPrioritySum = 0;
+        QDateTime currentDateTime = QDateTime::currentDateTime();
         int nChildren = 0;
-        //int nInputSum = 0;
+        int currentDay = QDateTime::currentDateTimeUtc().toString("dd").toInt();
+        int currentMonth = QDateTime::currentDateTimeUtc().toString("MM").toInt();
+        int currentYear = QDateTime::currentDateTimeUtc().toString("yyyy").toInt();
+        int lastMonth = QDateTime::currentDateTimeUtc().addMonths(-1).toString("M").toInt();
+
+        //Smartrewards snapshot date
+        QDateTime lastSmartrewardsSnapshotDateTime = QDateTime::currentDateTime();
+        QString dateFormat = QString("%1-%2-%3").arg(currentYear).arg(currentMonth).arg(SMARTREWARDS_DAY);
+        QString timeFormat = QString("%1:00:00").arg(SMARTREWARDS_UTC_HOUR);
+        lastSmartrewardsSnapshotDateTime.setDate(QDate::fromString(dateFormat, "yyyy-MM-dd"));
+        lastSmartrewardsSnapshotDateTime.setTime(QTime::fromString(timeFormat, "hh:mm:ss"));
+        if(currentDay < SMARTREWARDS_DAY){
+           dateFormat = QString("%1-%2-%3").arg(currentYear).arg(lastMonth).arg(SMARTREWARDS_DAY);
+           lastSmartrewardsSnapshotDateTime.setDate(QDate::fromString(dateFormat, "yyyy-MM-dd"));
+        }
+
         BOOST_FOREACH(const COutput& out, coins.second) {
-            //int nInputSize = 0;
+
             nSum += out.tx->vout[out.i].nValue;
             nChildren++;
  
-            // address
+            //address
             CTxDestination outputAddress;
             QString sAddress = "";
             if(ExtractDestination(out.tx->vout[out.i].scriptPubKey, outputAddress))
             {
                 sAddress = QString::fromStdString(CBitcoinAddress(outputAddress).ToString());
             }
- 
- 
+
+            //tx date
+            QDateTime txDateTime = QDateTime::fromTime_t((qint32)out.tx->GetTxTime());
+
+            if(txDateTime.toUTC() > lastSmartrewardsSnapshotDateTime){
+                //discard
+            }else{
+                //sum
+            }
+
             ui->tableWidget->setItem(nNewRow, 0, new QTableWidgetItem(sWalletLabel));
             ui->tableWidget->setItem(nNewRow, 1, new QTableWidgetItem(sWalletAddress));
-            ui->tableWidget->setItem(nNewRow, 2, new QTableWidgetItem(BitcoinUnits::format(nDisplayUnit, out.tx->vout[out.i].nValue)));
- 
+            ui->tableWidget->setItem(nNewRow, 2, new QTableWidgetItem(BitcoinUnits::format(nDisplayUnit, nSum)));
+
+            //eligible rewards
+            if(nSum >= SMARTREWARDS_MINIMUM_AMOUNT){
+                 ui->tableWidget->setItem(nNewRow, 3, new QTableWidgetItem(BitcoinUnits::format(nDisplayUnit, nSum)));
+            }else{
+                 ui->tableWidget->setItem(nNewRow, 3, new QTableWidgetItem(BitcoinUnits::format(nDisplayUnit, 0)));
+            }
+
         }
         nNewRow++;
  
