5d911f1cb55ebf19852264caafe6e62c583aafe9
Proteus <pro@proteanx.com>
2018-01-09 01:50:11 -0500

    fix compile

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -1984,7 +1984,7 @@ void CWallet::AvailableCoins(vector <COutput> &vCoins, bool fOnlyConfirmed, cons
                 //LogPrintf("mine != ISMINE_NO = %s\n", mine != ISMINE_NO);
                 //LogPrintf("(!IsLockedCoin((*it).first, i) || nCoinType == ONLY_10000) = %s\n", (!IsLockedCoin((*it).first, i) || nCoinType == ONLY_10000));
                 //LogPrintf("(pcoin->vout[i].nValue > nMinimumInputValue) = %s\n", (pcoin->vout[i].nValue > nMinimumInputValue));
-                //LogPrintf("!coinControl = %s\n", (
+                LogPrintf("!coinControl = %s\n", (
                         !coinControl ||
                         !coinControl->HasSelected() ||
                         coinControl->fAllowOtherInputs ||
