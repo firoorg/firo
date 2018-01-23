908c3edb95a9f4b0ff674c155c78df022b08ba72
Holger Schinzel <holger.schinzel@gmx.de>
2018-01-23 12:11:49 +0100

    Add a mutex lock to protect CNode::nRefCount

--- src/net.h
+++ src/net.h
@@ -469,6 +469,8 @@ private:
     static uint64_t nMaxOutboundLimit;
     static uint64_t nMaxOutboundTimeframe;
 
+    CCriticalSection cs_nRefCount;
+
     CNode(const CNode&);
     void operator=(const CNode&);
 
@@ -482,6 +484,7 @@ public:
 
     int GetRefCount()
     {
+        LOCK(cs_nRefCount);
         assert(nRefCount >= 0);
         return nRefCount;
     }
@@ -508,12 +511,14 @@ public:
 
     CNode* AddRef()
     {
+        LOCK(cs_nRefCount);
         nRefCount++;
         return this;
     }
 
     void Release()
     {
+        LOCK(cs_nRefCount);
         nRefCount--;
     }
 
