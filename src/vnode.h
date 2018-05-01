e921c23e3210efe6d4f58836a2cd5fb946902e0d
Lukas-Christian Schulze <lukas.schulze@gmail.com>
2018-05-01 20:44:31 +0200

    static const int VNODE_COIN_REQUIRED               = 3750;

--- src/vnode.h
+++ src/vnode.h
@@ -22,7 +22,7 @@ static const int VNODE_MIN_MNP_SECONDS             =  10 * 60; //PRE_ENABLE_TIME
 static const int VNODE_EXPIRATION_SECONDS          =  65 * 60;
 static const int VNODE_WATCHDOG_MAX_SECONDS        = 120 * 60;
 static const int VNODE_NEW_START_REQUIRED_SECONDS  = 180 * 60;
-static const int VNODE_COIN_REQUIRED  = 1000;
+static const int VNODE_COIN_REQUIRED               = 3750;
 
 static const int VNODE_POSE_BAN_MAX_SCORE          = 5;
 //
