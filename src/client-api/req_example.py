7f4732d8bda2c5930dc2a8eb0d3c5cf35ca5654a
riordant <riordant@tcd.ie>
2018-06-05 15:09:20 +0100

    Add request example. remove extra logs

---
+++ src/client-api/req_example.py
@@ -0,0 +1,30 @@
+#
+#   Request-reply client in Python
+#   Connects REQ socket to tcp://localhost:5557
+#   Sends json request to zcoind
+#
+import zmq
+import json
+
+#  Prepare our context and sockets
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://localhost:5557")
+
+message_input_list = b"{ \
+                    \"type\": \"getblock\", \
+                    \"payload\": [ \
+                        \"56b0558e170b2184ee0dbd053f517507dbaf4ad9289ba027dca62b6c03fc8645\" \
+                     ] \
+                  }";
+
+message_input_object = b"{ \
+                    \"type\": \"getaddressbalance\", \
+                    \"payload\": { \
+                        \"addresses\": [\"aQ18FBVFtnueucZKeVg4srhmzbpAeb1KoN\"] \
+                     } \
+                  }";
+                  
+socket.send(message_input_object)
+message = socket.recv()
+print("Received reply [%s]" % (message))
