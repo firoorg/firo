f51776ddf4e49aa4fe21b3ba7e097da310c0e822
riordant <riordant@tcd.ie>
2018-06-15 12:42:54 +0200

    Update request format

--- src/client-api/req_example.py
+++ src/client-api/req_example.py
@@ -6,30 +6,33 @@
 import zmq
 import json
 
+MAINNET = "15557"
+TESTNET = "15558"
+
 #  Prepare our context and sockets
 context = zmq.Context()
 socket = context.socket(zmq.REQ)
-socket.connect("tcp://localhost:5557")
+socket.connect("tcp://localhost:" + MAINNET)
 
 message_input_pr = b"{ \
-                    \"type\": \"getpaymentrequest\", \
-                    \"payload\": [ \
-                        \"4000\", \
-                        \"Joern's Payment Request\",\
-                        \"this is a payment request for Joern.\" \
-                     ] \
+                    \"type\": \"create-payment-request\", \
+                    \"data\": { \
+                        \"amount\": \"4000\", \
+                        \"label\": \"Joern's Payment Request\",\
+                        \"msg\": \"this is a payment request for Joern.\" \
+                     } \
                   }";
 
 message_input_list = b"{ \
                     \"type\": \"getblock\", \
-                    \"payload\": [ \
+                    \"data\": { \
                         \"56b0558e170b2184ee0dbd053f517507dbaf4ad9289ba027dca62b6c03fc8645\" \
-                     ] \
+                     } \
                   }";
 
 message_input_object = b"{ \
                     \"type\": \"getaddressbalance\", \
-                    \"payload\": { \
+                    \"data\": { \
                         \"addresses\": [\"aQ18FBVFtnueucZKeVg4srhmzbpAeb1KoN\"] \
                      } \
                   }";
