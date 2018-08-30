marcusfox0/polis

Implementation of ProRegTx with basic validation (no processing)

src/evo/providertx.h
@@ -1,0 +1,60 @@
+// Copyright (c) 2017 The Dash Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef DASH_PROVIDERTX_H
+#define DASH_PROVIDERTX_H
+
+#include "primitives/transaction.h"
+#include "consensus/validation.h"
+
+#include "netaddress.h"
+#include "pubkey.h"
+
+class CBlockIndex;
+class UniValue;
+
+class CProRegTx
+{
+public:
+    static const int CURRENT_VERSION = 1;
+
+public:
+    uint16_t nVersion{CURRENT_VERSION}; // message version
+    int32_t nProtocolVersion{0};
+    uint32_t nCollateralIndex{(uint32_t) - 1};
+    CService addr;
+    CKeyID keyIDOwner;
+    CKeyID keyIDOperator;
+    CKeyID keyIDVoting;
+    uint16_t operatorReward{0};
+    CScript scriptPayout;
+    uint256 inputsHash; // replay protection
+    std::vector<unsigned char> vchSig;
+
+public:
+    ADD_SERIALIZE_METHODS;
+
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action)
+    {
+        READWRITE(nVersion);
+        READWRITE(nProtocolVersion);
+        READWRITE(nCollateralIndex);
+        READWRITE(addr);
+        READWRITE(keyIDOwner);
+        READWRITE(keyIDOperator);
+        READWRITE(keyIDVoting);
+        READWRITE(*(CScriptBase*)(&scriptPayout));
+        READWRITE(operatorReward);
+        READWRITE(inputsHash);
+        READWRITE(vchSig);
+    }
+
+    std::string ToString() const;
+    void ToJson(UniValue& obj) const;
+};
+
+bool CheckProRegTx(const CTransaction& tx, const CBlockIndex* pindex, CValidationState& state);
+
+#endif//DASH_PROVIDERTX_H

src/evo/specialtx.cpp
@@ -17,6 +17,8 @@ bool CheckSpecialTx(const CTransaction& tx, const CBlockIndex* pindex, CValidati
         return true;
 
     switch (tx.nType) {
+        case TRANSACTION_PROVIDER_REGISTER:
+            return CheckProRegTx(tx, pindex, state);
     }
 
     return state.DoS(100, false, REJECT_INVALID, "bad-tx-type");
@@ -28,6 +30,8 @@ bool ProcessSpecialTx(const CTransaction& tx, const CBlockIndex* pindex, CValida
         return true;
 
     switch (tx.nType) {
+        case TRANSACTION_PROVIDER_REGISTER:
+            return true; // handled in batches per block
     }
 
     return state.DoS(100, false, REJECT_INVALID, "bad-tx-type");
@@ -39,6 +43,8 @@ bool UndoSpecialTx(const CTransaction& tx, const CBlockIndex* pindex)
         return true;
 
     switch (tx.nType) {
+        case TRANSACTION_PROVIDER_REGISTER:
+            return true; // handled in batches per block
     }
 
     return false;

firoorg/firo

src/evo/providertx.h
@@ -1,0 +1,62 @@
+// Copyright (c) 2018 The Dash Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef DASH_PROVIDERTX_H
+#define DASH_PROVIDERTX_H
+
+#include "primitives/transaction.h"
+#include "consensus/validation.h"
+
+#include "netaddress.h"
+#include "pubkey.h"
+
+class CBlockIndex;
+class UniValue;
+
+class CProRegTx
+{
+public:
+    static const uint16_t CURRENT_VERSION = 1;
+
+public:
+    uint16_t nVersion{CURRENT_VERSION}; // message version
+    int32_t nProtocolVersion{0};
+    uint32_t nCollateralIndex{(uint32_t) - 1};
+    CService addr;
+    CKeyID keyIDOwner;
+    CKeyID keyIDOperator;
+    CKeyID keyIDVoting;
+    uint16_t nOperatorReward{0};
+    CScript scriptPayout;
+    uint256 inputsHash; // replay protection
+    std::vector<unsigned char> vchSig;
+
+public:
+    ADD_SERIALIZE_METHODS;
+
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action)
+    {
+        READWRITE(nVersion);
+        READWRITE(nProtocolVersion);
+        READWRITE(nCollateralIndex);
+        READWRITE(addr);
+        READWRITE(keyIDOwner);
+        READWRITE(keyIDOperator);
+        READWRITE(keyIDVoting);
+        READWRITE(*(CScriptBase*)(&scriptPayout));
+        READWRITE(nOperatorReward);
+        READWRITE(inputsHash);
+        if (!(s.GetType() & SER_GETHASH)) {
+            READWRITE(vchSig);
+        }
+    }
+
+    std::string ToString() const;
+    void ToJson(UniValue& obj) const;
+};
+
+bool CheckProRegTx(const CTransaction& tx, const CBlockIndex* pindexPrev, CValidationState& state);
+
+#endif//DASH_PROVIDERTX_H

src/evo/specialtx.cpp
@@ -24,6 +24,8 @@ bool CheckSpecialTx(const CTransaction& tx, const CBlockIndex* pindexPrev, CVali
     }
 
     switch (tx.nType) {
+        case TRANSACTION_PROVIDER_REGISTER:
+            return CheckProRegTx(tx, pindexPrev, state);
     }
 
     return state.DoS(10, false, REJECT_INVALID, "bad-tx-type");
@@ -35,6 +37,8 @@ bool ProcessSpecialTx(const CTransaction& tx, const CBlockIndex* pindex, CValida
         return true;
 
     switch (tx.nType) {
+        case TRANSACTION_PROVIDER_REGISTER:
+            return true; // handled in batches per block
     }
 
     return state.DoS(100, false, REJECT_INVALID, "bad-tx-type");
@@ -46,6 +50,8 @@ bool UndoSpecialTx(const CTransaction& tx, const CBlockIndex* pindex)
         return true;
 
     switch (tx.nType) {
+        case TRANSACTION_PROVIDER_REGISTER:
+            return true; // handled in batches per block
     }
 
     return false;