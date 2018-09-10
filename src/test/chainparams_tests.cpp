320465d81da8125329cc0351aa76fd30f1a24529
Andrey Bezrukov <andreyvbezrukov@gmail.com>
2018-09-10 12:58:00 +0400

    Polishing

--- src/test/chainparams_tests.cpp
+++ src/test/chainparams_tests.cpp
@@ -19,7 +19,7 @@ BOOST_AUTO_TEST_CASE(regtest_chainparams_verification)
 //        std::cout << "zcoin regtest genesisBlock hash: " << consensus.hashGenesisBlock.ToString() << std::endl;
 //        std::cout << "zcoin regtest hashMerkleRoot hash: " << genesis.hashMerkleRoot.ToString() << std::endl;
 
-    BOOST_CHECK(consensus.hashGenesisBlock == uint256S("0xee98d9dc0da1f3378edeeed1edcaf7d657952257d4700d594eb7c08ac1d6fa9a"));
+    BOOST_CHECK(consensus.hashGenesisBlock == uint256S("0x3a84562b4f3837ee2550fe0ecb35c5e65f8c0d929c60b8c876d5712117161ed6"));
     BOOST_CHECK(genesis.hashMerkleRoot == uint256S("0x25b361d60bc7a66b311e72389bf5d9add911c735102bcb6425f63aceeff5b7b8"));
 
     BOOST_CHECK(genesis.nNonce == 3);
