2885e56a7afcd8153ec17dd07f65ff5ea5cf5ed8
Martun Karapetyan <martun.karapetyan>
2018-10-12 03:09:35 -0700

    Removed a few dandelion logs.

--- src/net.h
+++ src/net.h
@@ -585,7 +585,7 @@ public:
     void PushInventory(const CInv& inv)
     {
         LOCK(cs_inventory);
-        LogPrintf("Pushing inventory %s to %s.\n", inv.ToString(), addr.ToString());
+        // LogPrintf("Pushing inventory %s to %s.\n", inv.ToString(), addr.ToString());
         if (inv.type == MSG_TX) {
             if (!filterInventoryKnown.contains(inv.hash)) {
                 setInventoryTxToSend.insert(inv.hash);
