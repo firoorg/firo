50d30662366ffda51007ca16482d5e0ef1e9eeee
tadhg <riordant@tcd.ie>
2018-10-18 14:45:51 +0200

    OTAC authorization for dumpwallet and dumpprivkey (#291)
    * Implemented 2-step auth of dumpprivkey and dumpwallet

--- src/rpc/server.cpp
+++ src/rpc/server.cpp
@@ -472,7 +472,7 @@ std::vector<std::string> CRPCTable::listCommands() const
 
 std::string HelpExampleCli(const std::string& methodname, const std::string& args)
 {
-    return "> bitcoin-cli " + methodname + " " + args + "\n";
+    return "> zcoin-cli " + methodname + " " + args + "\n";
 }
 
 std::string HelpExampleRpc(const std::string& methodname, const std::string& args)
