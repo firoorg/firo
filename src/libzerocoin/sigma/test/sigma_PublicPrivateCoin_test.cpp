23fc70b32543dbc1853b19bd9b65765ed70da827
thebevrishot <panu.suk@satang.com>
2018-11-28 13:58:12 +0700

    Ported sigma_PublicPrivateCoin_test

---
+++ src/libzerocoin/sigma/test/sigma_PublicPrivateCoin_test.cpp
@@ -0,0 +1,27 @@
+#include <boost/test/unit_test.hpp>
+#include <libzerocoin/sigma/Coin.h>
+#include <libzerocoin/sigma/Params.h>
+#include <streams.h>
+
+using namespace sigma;
+using namespace std;
+
+BOOST_AUTO_TEST_CASE(sigma_PublicPrivateCoin_test)
+{
+    //creating params
+    ParamsV3* params = sigma::ParamsV3::get_default();
+
+    //testing public coin and serialize
+    secp_primitives::GroupElement coin;
+    coin.randomize();
+    PublicCoinV3 publicCoin(coin, ZQ_GOLDWASSER);
+    CDataStream serializedCoin(SER_NETWORK, PROTOCOL_VERSION);
+    serializedCoin << publicCoin;
+    PublicCoinV3 publicCoinNew(serializedCoin);
+    BOOST_CHECK(publicCoin == publicCoinNew);
+
+    // mint test
+    PrivateCoinV3 newCoin(params);
+    PublicCoinV3 pubCoin = newCoin.getPublicCoin();
+    BOOST_CHECK(pubCoin.validate());
+}
