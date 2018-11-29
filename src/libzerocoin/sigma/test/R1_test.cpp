511f344c69e711c5a45546828e605110af5d3687
thebevrishot <panu.suk@satang.com>
2018-11-29 15:10:14 +0700

    Port sigma_R1 and sigma_serialize tests to boost test

---
+++ src/libzerocoin/sigma/test/R1_test.cpp
@@ -0,0 +1,194 @@
+#include <boost/test/unit_test.hpp>
+
+#include <libzerocoin/sigma/R1Proof.h>
+#include <libzerocoin/sigma/R1ProofGenerator.h>
+#include <libzerocoin/sigma/R1ProofVerifier.h>
+#include <stdlib.h>
+
+bool test(secp_primitives::GroupElement& g, std::vector<secp_primitives::GroupElement>& h_, std::vector<secp_primitives::Scalar>& b, int n, int m){
+
+    secp_primitives::Scalar r;
+    r.randomize();
+    zcoin_common::GeneratorVector<secp_primitives::Scalar, secp_primitives::GroupElement> h_gens(h_);
+    sigma::R1ProofGenerator<secp_primitives::Scalar, secp_primitives::GroupElement> prover(g, h_gens, b, r, n, m);
+    sigma::R1Proof<secp_primitives::Scalar, secp_primitives::GroupElement> proof;
+    prover.proof(proof);
+
+    sigma::R1ProofVerifier<secp_primitives::Scalar, secp_primitives::GroupElement> verifier(g,h_gens,prover.get_B(), n, m);
+    return verifier.verify(proof);
+}
+
+BOOST_AUTO_TEST_SUITE(sigma_R1_test)
+
+BOOST_AUTO_TEST_CASE(fixed_size_test)
+{
+    int n = 4;
+    int m = 16;
+    secp_primitives::GroupElement g;
+    g.randomize();
+    std::vector<secp_primitives::GroupElement> h_;
+    std::vector<secp_primitives::Scalar> b;
+    secp_primitives::GroupElement h;
+    for(int i = 0; i < m; ++i) {
+        h.randomize();
+        h_.push_back(h);
+        b.push_back(secp_primitives::Scalar(uint64_t(1)));
+        for(int j = 1; j < n; ++j){
+            h.randomize();
+            h_.push_back(h);
+            b.push_back(secp_primitives::Scalar(uint64_t(0)));
+
+        }
+    }
+
+    BOOST_CHECK(test(g, h_, b, n, m));
+}
+
+BOOST_AUTO_TEST_CASE(random_size_test)
+{
+    int n = rand() % 64 + 16;
+    int m = rand() % 32 + 16;
+    secp_primitives::GroupElement g;
+    g.randomize();
+    std::vector<secp_primitives::GroupElement> h_;
+    std::vector<secp_primitives::Scalar> b;
+    for(int i = 0; i < m; ++i) {
+        secp_primitives::GroupElement h;
+        b.push_back(secp_primitives::Scalar(uint64_t(1)));
+        for(int j = 1; j < n; ++j){
+            h.randomize();
+            h_.push_back(h);
+            b.push_back(secp_primitives::Scalar(uint64_t(0)));
+
+        }
+    }
+
+    BOOST_CHECK(test(g, h_, b, n, m));
+}
+
+
+BOOST_AUTO_TEST_CASE(all_positions)
+{
+    int n = 32;
+    int m = 16;
+    secp_primitives::GroupElement g;
+    std::vector <secp_primitives::GroupElement> h_;
+    std::vector <secp_primitives::Scalar> b;
+    secp_primitives::GroupElement h;
+    for(int k = 0; k < n; ++k){
+        g.randomize();
+        for(int i = 0;i< m ;++i) {
+            for(int j = 0;j < n;++j){
+                h.randomize();
+                h_.push_back(h);
+                if(j == k)
+                    b.push_back(secp_primitives::Scalar(uint64_t(1)));
+                else
+                    b.push_back(secp_primitives::Scalar(uint64_t(0)));
+            }
+        }
+        BOOST_CHECK(test(g, h_, b, n, m));
+        h_.clear();
+        b.clear();
+    }
+}
+
+BOOST_AUTO_TEST_CASE(one_in_random_position)
+{
+    int n = 32;
+    int m = 16;
+    int k = rand() % n;
+    secp_primitives::GroupElement g;
+    std::vector <secp_primitives::GroupElement> h_;
+    std::vector <secp_primitives::Scalar> b;
+    secp_primitives::GroupElement h;
+    g.randomize();
+    for(int i = 0;i < m;++i) {
+        k = rand() % n;
+        for(int j = 0;j < n;++j){
+            h.randomize();
+            h_.push_back(h);
+            if(j == k)
+                b.push_back(secp_primitives::Scalar(uint64_t(1)));
+            else
+                b.push_back(secp_primitives::Scalar(uint64_t(0)));
+        }
+    }
+    BOOST_CHECK(test(g, h_, b, n, m));
+}
+
+
+BOOST_AUTO_TEST_CASE(all_0s_in_random_row)
+{
+    int n = 32;
+    int m = 16;
+    int k = rand() % m;
+    secp_primitives::GroupElement g;
+    std::vector <secp_primitives::GroupElement> h_;
+    std::vector <secp_primitives::Scalar> b;
+    secp_primitives::GroupElement h;
+    g.randomize();
+    for(int i = 0;i < m;++i) {
+        for(int j = 0;j < n;++j){
+            h.randomize();
+            h_.push_back(h);
+            if(i != k)
+                b.push_back(secp_primitives::Scalar(uint64_t(1)));
+            else
+                b.push_back(secp_primitives::Scalar(uint64_t(0)));
+        }
+    }
+    BOOST_CHECK(!test(g, h_, b, n, m)); // expect false
+}
+
+BOOST_AUTO_TEST_CASE(all_1s_in_random_row)
+{
+    int n = 32;
+    int m = 16;
+    int k = rand() % m;
+    secp_primitives::GroupElement g;
+    std::vector <secp_primitives::GroupElement> h_;
+    std::vector <secp_primitives::Scalar> b;
+    secp_primitives::GroupElement h;
+    g.randomize();
+    for(int i = 0;i < m;++i) {
+        for(int j = 0;j < n;++j){
+            h.randomize();
+            h_.push_back(h);
+            if(i == k)
+                b.push_back(secp_primitives::Scalar(uint64_t(1)));
+            else
+                b.push_back(secp_primitives::Scalar(uint64_t(0)));
+        }
+    }
+    BOOST_CHECK(!test(g, h_, b, n, m)); // expect false
+}
+
+
+BOOST_AUTO_TEST_CASE(two_1s_in_random_row)
+{
+    int n = 32;
+    int m = 16;
+    int k = rand() % n;
+    secp_primitives::GroupElement g;
+    std::vector <secp_primitives::GroupElement> h_;
+    std::vector <secp_primitives::Scalar> b;
+    secp_primitives::GroupElement h;
+    g.randomize();
+    for(int i = 0;i < m;++i) {
+        for(int j = 0;j < n;++j){
+            h.randomize();
+            h_.push_back(h);
+            if(j == k){
+                b.push_back(secp_primitives::Scalar(uint64_t(1)));
+                b.push_back(secp_primitives::Scalar(uint64_t(1)));
+                ++j;
+            }
+            else
+                b.push_back(secp_primitives::Scalar(uint64_t(0)));
+        }
+    }
+    BOOST_CHECK(!test(g, h_, b, n, m)); // expect false
+}
+
+BOOST_AUTO_TEST_SUITE_END()
---
+++ src/libzerocoin/sigma/test/serialize_test.cpp
@@ -0,0 +1,94 @@
+#include <boost/test/unit_test.hpp>
+
+#include <libzerocoin/sigma/SigmaPlusProver.h>
+#include <libzerocoin/sigma/SigmaPlusVerifier.h>
+
+BOOST_AUTO_TEST_SUITE(sigma_serialize_tests)
+
+BOOST_AUTO_TEST_CASE(group_element_serialize)
+{
+    secp_primitives::GroupElement initial;
+    initial.randomize();
+    unsigned char buffer [initial.memoryRequired()];
+    initial.serialize(buffer);
+    secp_primitives::GroupElement resulted;
+    resulted.deserialize(buffer);
+    BOOST_CHECK(initial == resulted);
+}
+
+BOOST_AUTO_TEST_CASE(group_element_serialize_infinity)
+{
+    secp_primitives::GroupElement initial;
+    unsigned char buffer [initial.memoryRequired()];
+    initial.serialize(buffer);
+    secp_primitives::GroupElement resulted;
+    resulted.deserialize(buffer);
+    BOOST_CHECK(initial == resulted);
+}
+
+BOOST_AUTO_TEST_CASE(scalar_serialize)
+{
+    secp_primitives::Scalar initial;
+    initial.randomize();
+    unsigned char buffer [initial.memoryRequired()];
+    initial.serialize(buffer);
+    secp_primitives::Scalar resulted;
+    resulted.deserialize(buffer);
+    BOOST_CHECK(initial == resulted);
+}
+
+BOOST_AUTO_TEST_CASE(proof_serialize)
+{
+    int N = 16;
+    int n = 4;
+    int index = 0;
+
+    int m = (int)(log(N) / log(n));;
+
+    secp_primitives::GroupElement g;
+    g.randomize();
+    std::vector<secp_primitives::GroupElement> h_gens;
+    h_gens.resize(n * m);
+    for(int i = 0; i < n * m; ++i ){
+        h_gens[i].randomize();
+    }
+    secp_primitives::Scalar r;
+    r.randomize();
+    sigma::SigmaPlusProver<secp_primitives::Scalar,secp_primitives::GroupElement> prover(g,h_gens, n, m);
+
+    std::vector<secp_primitives::GroupElement> commits;
+    for(int i = 0; i < N; ++i){
+        if(i == index){
+            secp_primitives::GroupElement c;
+            secp_primitives::Scalar zero(uint64_t(0));
+            c = sigma::SigmaPrimitives<secp_primitives::Scalar,secp_primitives::GroupElement>::commit(g, zero, h_gens[0], r);
+            commits.push_back(c);
+
+        }
+        else{
+            commits.push_back(secp_primitives::GroupElement());
+            commits[i].randomize();
+        }
+    }
+
+    sigma::SigmaPlusProof<secp_primitives::Scalar,secp_primitives::GroupElement> initial_proof;
+
+    prover.proof(commits, index, r, initial_proof);
+
+    unsigned char buffer [initial_proof.memoryRequired()];
+    initial_proof.serialize(buffer);
+    sigma::SigmaPlusProof<secp_primitives::Scalar,secp_primitives::GroupElement> resulted_proof;
+    resulted_proof.deserialize(buffer, n, m);
+
+    BOOST_CHECK(initial_proof.B_ == resulted_proof.B_);
+    BOOST_CHECK(initial_proof.r1Proof_.A_ == resulted_proof.r1Proof_.A_);
+    BOOST_CHECK(initial_proof.r1Proof_.C_ == resulted_proof.r1Proof_.C_);
+    BOOST_CHECK(initial_proof.r1Proof_.D_ == resulted_proof.r1Proof_.D_);
+    BOOST_CHECK(initial_proof.r1Proof_.f_ == resulted_proof.r1Proof_.f_);
+    BOOST_CHECK(initial_proof.r1Proof_.ZA_ == resulted_proof.r1Proof_.ZA_);
+    BOOST_CHECK(initial_proof.r1Proof_.ZC_ == resulted_proof.r1Proof_.ZC_);
+    BOOST_CHECK(initial_proof.Gk_ == resulted_proof.Gk_);
+    BOOST_CHECK(initial_proof.z_ == resulted_proof.z_);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
