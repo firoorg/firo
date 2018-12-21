01a7c3ec9ca4a8a6731a88c665a77b42f9b35c13
thebevrishot <panu.suk@satang.com>
2018-12-21 14:32:34 +0700

    Add coin serialize deserialize priv coin

--- src/libzerocoin/sigma/test/coin_tests.cpp
+++ src/libzerocoin/sigma/test/coin_tests.cpp
@@ -32,4 +32,27 @@ BOOST_AUTO_TEST_CASE(pubcoin_validate)
     BOOST_CHECK(pubcoin.validate());
 }
 
+BOOST_AUTO_TEST_CASE(getter_setter_priv)
+{
+    auto params = sigma::ParamsV3::get_default();
+
+    sigma::PrivateCoinV3 privcoin(params);
+    sigma::PrivateCoinV3 new_privcoin(params);
+
+    BOOST_CHECK(privcoin.getPublicCoin() != new_privcoin.getPublicCoin());
+    BOOST_CHECK(privcoin.getSerialNumber() != new_privcoin.getSerialNumber());
+    BOOST_CHECK(privcoin.getRandomness() != new_privcoin.getRandomness());
+    BOOST_CHECK(privcoin.getVersion() == new_privcoin.getVersion());
+
+    new_privcoin.setPublicCoin(privcoin.getPublicCoin());
+    new_privcoin.setRandomness(privcoin.getRandomness());
+    new_privcoin.setSerialNumber(privcoin.getSerialNumber());
+    new_privcoin.setVersion(2);
+
+    BOOST_CHECK(privcoin.getPublicCoin() == new_privcoin.getPublicCoin());
+    BOOST_CHECK(privcoin.getSerialNumber() == new_privcoin.getSerialNumber());
+    BOOST_CHECK(privcoin.getRandomness() == new_privcoin.getRandomness());
+    BOOST_CHECK(new_privcoin.getVersion() == 2);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
