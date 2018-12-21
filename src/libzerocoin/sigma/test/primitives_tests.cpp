aa613bc92f5e7ab7676e34ec9226ce2e48483bed
thebevrishot <panu.suk@satang.com>
2018-12-21 13:27:25 +0700

    Add vector commitments test

--- src/libzerocoin/sigma/test/primitives_tests.cpp
+++ src/libzerocoin/sigma/test/primitives_tests.cpp
@@ -88,6 +88,37 @@ BOOST_AUTO_TEST_CASE(commit2_test)
     BOOST_TEST(expected == resulted.tostring());
 }
 
+BOOST_AUTO_TEST_CASE(commit2_vs_test)
+{
+    // g*r+h*x == expected
+    secp_primitives::GroupElement g("9216064434961179932092223867844635691966339998754536116709681652691785432045",
+        "33986433546870000256104618635743654523665060392313886665479090285075695067131");
+    secp_primitives::GroupElement h1("50204771751011461524623624559944050110546921468100198079190811223951215371253",
+        "71960464583475414858258501028406090652116947054627619400863446545880957517934");
+    secp_primitives::GroupElement h2("7143275630583997983432964947790981761478339235433352888289260750805571589245",
+        "11700086115751491157288596384709446578503357013980342842588483174733971680454");
+    std::vector<secp_primitives::GroupElement> h_;
+    h_.push_back(h1);
+    h_.push_back(h2);
+
+    zcoin_common::GeneratorVector<secp_primitives::Scalar, secp_primitives::GroupElement> h_gens(h_);
+
+    secp_primitives::Scalar r(10);
+
+    std::vector<secp_primitives::Scalar> x_;
+    x_.push_back(secp_primitives::Scalar(20));
+    x_.push_back(secp_primitives::Scalar(30));
+
+    std::string expected;
+    expected = std::string("(70526180889965554490147039875158729645670819462806403227314362575089997039220,"
+			"2222570326551458324082940637823059881421581507683863189059174257118412428198)");
+
+    secp_primitives::GroupElement resulted;
+    sigma::SigmaPrimitives<secp_primitives::Scalar,secp_primitives::GroupElement>::commit(g,h_,x_,r,resulted);
+
+    BOOST_TEST(expected == resulted.tostring());
+}
+
 BOOST_AUTO_TEST_CASE(commit2_homomorphic_test)
 {
     // commit(x1,x2:r)+commit(y1,y2:q) == commit(x1+y1,x2+y2:r+q)
