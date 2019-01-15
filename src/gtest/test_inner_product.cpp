154b1435b300b9e79c1d8da54f4e719ad64eec72
petrosyan.levon93@gmail.com <petrosyan.levon93@gmail.com>
2019-01-15 13:03:45 +0400

    Added test to inner product

---
+++ src/gtest/test_inner_product.cpp
@@ -0,0 +1,91 @@
+#include <gtest/gtest.h>
+#include <nextgen/InnerProductProoveGenerator.h>
+#include <nextgen/InnerProductProofVerifier.h>
+
+void generate(
+        int n,
+        std::vector <secp_primitives::GroupElement>& gens_g,
+        std::vector <secp_primitives::GroupElement>& gens_h,
+        std::vector <secp_primitives::Scalar>& a,
+        std::vector <secp_primitives::Scalar>& b,
+        secp_primitives::GroupElement& u_) {
+
+    //creating generators g, h
+    for (int i = 0; i < n; ++i) {
+        secp_primitives::GroupElement g;
+        secp_primitives::GroupElement h;
+        g.randomize();
+        gens_g.push_back(g);
+        h.randomize();
+        gens_h.push_back(h);
+    }
+    //creating group element u
+    u_.randomize();
+    //    //create a and b vectors
+    for (int i = 0; i < n; ++i) {
+        secp_primitives::Scalar a_;
+        secp_primitives::Scalar b_;
+        a_.randomize();
+        b_.randomize();
+        a.emplace_back(a_);
+        b.emplace_back(b_);
+    }
+}
+
+TEST(InnerProductProoveGeneratorTest, proof_verify)
+{
+    int n = 32;
+    std::vector <secp_primitives::GroupElement> gens_g;
+    std::vector <secp_primitives::GroupElement> gens_h;
+    std::vector <secp_primitives::Scalar> a;
+    std::vector <secp_primitives::Scalar> b;
+    secp_primitives::GroupElement u_;
+    // generating needed objects
+    generate(n, gens_g, gens_h, a, b, u_);
+    //    creating generator vectors g, h
+    zcoin_common::GeneratorVector <secp_primitives::Scalar, secp_primitives::GroupElement> g_(gens_g);
+    zcoin_common::GeneratorVector <secp_primitives::Scalar, secp_primitives::GroupElement> h_(gens_h);
+
+    //    //creating proof genertor
+    nextgen::InnerProductProoveGenerator<secp_primitives::Scalar, secp_primitives::GroupElement> prooveGenerator(g_, h_, u_);
+
+    //////    //generating proof
+    nextgen::InnerProductProof<secp_primitives::Scalar, secp_primitives::GroupElement> proof;
+    prooveGenerator.generate_proof(a, b, proof);
+
+    ////    //create verifier
+    nextgen::InnerProductProofVerifier<secp_primitives::Scalar, secp_primitives::GroupElement> verifier(g_, h_, u_, prooveGenerator.get_P());
+
+    EXPECT_TRUE(verifier.verify(proof));
+
+}
+
+TEST(InnerProductProoveGeneratorTest, fake_proof_notVerify)
+{
+    int n = 32;
+    std::vector <secp_primitives::GroupElement> gens_g;
+    std::vector <secp_primitives::GroupElement> gens_h;
+    std::vector <secp_primitives::Scalar> a;
+    std::vector <secp_primitives::Scalar> b;
+    secp_primitives::GroupElement u_;
+    // generating needed objects
+    generate(n, gens_g, gens_h, a, b, u_);
+    //    creating generator vectors g, h
+    zcoin_common::GeneratorVector <secp_primitives::Scalar, secp_primitives::GroupElement> g_(gens_g);
+    zcoin_common::GeneratorVector <secp_primitives::Scalar, secp_primitives::GroupElement> h_(gens_h);
+
+    //    //creating proof genertor
+    nextgen::InnerProductProoveGenerator<secp_primitives::Scalar, secp_primitives::GroupElement> prooveGenerator(g_, h_, u_);
+
+    //////    //generating proof
+    nextgen::InnerProductProof<secp_primitives::Scalar, secp_primitives::GroupElement> proof;
+    prooveGenerator.generate_proof(a, b, proof);
+
+    ////    //create verifier with fake P
+    secp_primitives::GroupElement P;
+    P.randomize();
+    nextgen::InnerProductProofVerifier<secp_primitives::Scalar, secp_primitives::GroupElement> verifier(g_, h_, u_, P);
+
+    EXPECT_FALSE(verifier.verify(proof));
+
+}
