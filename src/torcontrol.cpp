61203a2af70b0a0bdef3b5a1a0dcdefcb13e538b
Luke Dashjr <luke-jr+git@utopios.org>
2019-03-23 05:43:49 +0000

    torcontrol: Use the default/standard network port for Tor hidden services, even if the internal port is set differently
    Currently, the hidden service is published on the same port as the public listening port.
    But if a non-standard port is configured, this can be used to guess (pretty reliably) that the public IP and the hidden service are the same node.

--- src/torcontrol.cpp
+++ src/torcontrol.cpp
@@ -9,7 +9,7 @@
 #include "net.h"
 #include "util.h"
 #include "crypto/hmac_sha256.h"
-
+#include <chainparams.h>
 #include <vector>
 #include <deque>
 #include <set>
@@ -504,7 +504,7 @@ void TorController::add_onion_cb(TorControlConnection& _conn, const TorControlRe
             }
             return;
         }
-        service = LookupNumeric(std::string(service_id+".onion").c_str(), GetListenPort());
+        service = LookupNumeric(std::string(service_id+".onion").c_str(), Params().GetDefaultPort());
         LogPrintf("tor: Got service ID %s, advertising service %s\n", service_id, service.ToString());
         if (WriteBinaryFile(GetPrivateKeyFile(), private_key)) {
             LogPrint("tor", "tor: Cached service private key to %s\n", GetPrivateKeyFile());
@@ -538,10 +538,9 @@ void TorController::auth_cb(TorControlConnection& _conn, const TorControlReply& 
         if (private_key.empty()) // No private key, generate one
             private_key = "NEW:RSA1024"; // Explicitly request RSA1024 - see issue #9214
         // Request hidden service, redirect port.
-        // Note that the 'virtual' port doesn't have to be the same as our internal port, but this is just a convenient
-        // choice.  TODO; refactor the shutdown sequence some day.
-        _conn.Command(strprintf("ADD_ONION %s Port=%i,127.0.0.1:%i", private_key, GetListenPort(), GetListenPort()),
-            boost::bind(&TorController::add_onion_cb, this, _1, _2));
+        // Note that the 'virtual' port is always the default port to avoid decloaking nodes using other ports.
+        _conn.Command(strprintf("ADD_ONION %s Port=%i,127.0.0.1:%i", private_key, Params().GetDefaultPort(), GetListenPort()),
+            std::bind(&TorController::add_onion_cb, this, std::placeholders::_1, std::placeholders::_2));
     } else {
         LogPrintf("tor: Authentication failed\n");
     }
