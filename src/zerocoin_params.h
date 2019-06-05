6a3bc615160a18d0128c8c4c62ebf9acb7116a13
Andrey <andrey@zcoin.io>
2019-06-05 17:09:18 +0400

    Sigma start block adjusted

--- src/zerocoin_params.h
+++ src/zerocoin_params.h
@@ -39,7 +39,7 @@ static const int64_t DUST_HARD_LIMIT = 1000;   // 0.00001 XZC mininput
 #define ZC_V1_5_GRACEFUL_PERIOD			1500
 
 // Block after which sigma mints are activated.
-#define ZC_SIGMA_STARTING_BLOCK         300000
+#define ZC_SIGMA_STARTING_BLOCK         175600 //Approx July 11th, 2019, 8:00 AM UTC
 #define ZC_SIGMA_TESTNET_STARTING_BLOCK 50000
 
 // Number of blocks after ZC_SIGMA_STARTING_BLOCK during which we still accept zerocoin V2 mints into mempool.
