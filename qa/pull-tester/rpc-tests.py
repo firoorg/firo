434ec79bf7d0db2f9f48221b98d60692586c5eb5
Mariia Kren <mkren@intermedia.net>
2019-07-05 13:37:06 +0300

    Fixed typo: - lockedwallet

--- qa/pull-tester/rpc-tests.py
+++ qa/pull-tester/rpc-tests.py
@@ -146,7 +146,7 @@ testScripts = [
     # 'signmessages.py',
     # 'p2p-compactblocks.py',
     # 'nulldummy.py',
-    'sigma_remint_locked_wallet.py',
+    'sigma_remint_lockedwallet.py',
     'sigma_zapsigmamints.py',
     'sigma_remint.py',
     'sigma_remint_validation.py',
