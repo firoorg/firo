29941236b3e02b9c29007768e49a426a93a38d9f
Levon Petrosyan <petrosyan.levon93@gmail.com>
2019-07-15 14:41:25 +0400

    Condition changed

--- src/sigma/params.cpp
+++ src/sigma/params.cpp
@@ -11,7 +11,7 @@ Params* Params::get_default() {
         //fixing generator G;
         GroupElement g;
 
-        if(::Params().GetConsensus().IsMain()) {
+        if(!(::Params().GetConsensus().IsTestnet())) {
             unsigned char buff[32] = {0};
             GroupElement base;
             base.set_base_g();
