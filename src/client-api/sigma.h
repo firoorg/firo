d681949985acc58e773b3131edbfa1e76916d5da
riordant <riordant@tcd.ie>
2019-10-08 17:36:42 +0700

    Add sigma header

---
+++ src/client-api/sigma.h
@@ -0,0 +1,7 @@
+// Copyright (c) 2018 Tadhg Riordan Zcoin Developer
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "univalue.h"
+
+UniValue GetDenominations();
