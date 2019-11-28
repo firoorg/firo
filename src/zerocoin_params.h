467aeb67a4097c2c682005174e224e9c04e75dc8
Andrey <andrey@zcoin.io>
2019-11-28 23:39:10 +0400

    Added the comment

--- src/zerocoin_params.h
+++ src/zerocoin_params.h
@@ -43,7 +43,7 @@ static const int64_t DUST_HARD_LIMIT = 1000;   // 0.00001 XZC mininput
 #define ZC_SIGMA_TESTNET_STARTING_BLOCK 50000
 
 // Block after which anonymity sets are being padded.
-#define ZC_SIGMA_PADDING_BLOCK         220720 //Approx November 29th, 2019, 8:00 AM UTC
+#define ZC_SIGMA_PADDING_BLOCK         220720 //Approx December 5th 12PM UTC
 #define ZC_SIGMA_TESTNET_PADDING_BLOCK 110000
 
 //Block after whinch we are disabling sigma to enable after starting padding
