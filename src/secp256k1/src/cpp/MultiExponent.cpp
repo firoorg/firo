0f44e8951a553328b98bc98e83db9183451fdfd6
Peter Shugalev <peter@shugalev.com>
2019-12-02 10:07:32 +0300

    Switched to different way to deal with thread interruptions

--- src/secp256k1/src/cpp/MultiExponent.cpp
+++ src/secp256k1/src/cpp/MultiExponent.cpp
@@ -21,9 +21,6 @@
 #include <queue>
 #include <algorithm>
 
-// our code currently relies on boost disable_interruption. This will go away with core upgrade
-#include <boost/thread.hpp>
-
 // Simple implementation of thread pool
 
 namespace {
@@ -33,10 +30,11 @@ constexpr static int secondsBeforeThreadShutdown = 10;
 
 // Simple thread pool class for using multiple cores effeciently
 
+template <typename Result>
 class ParallelOpThreadPool {
 private:
     std::list<std::thread>                    threads;
-    std::queue<std::packaged_task<void()>>    task_queue;
+    std::queue<std::packaged_task<Result()>>  task_queue;
     std::mutex                                task_queue_mutex;
     std::condition_variable                   task_queue_condition;
 
@@ -45,7 +43,7 @@ private:
 
     void ThreadProc() {
         for (;;) {
-            std::packaged_task<void()> job;
+            std::packaged_task<Result()> job;
             {
                 std::unique_lock<std::mutex> lock(task_queue_mutex);
 
@@ -72,7 +70,7 @@ private:
     void StartThreads() {
         // should be called with mutex aquired
         // start missing threads
-        while(threads.size() < number_of_threads)
+        while (threads.size() < number_of_threads)
             threads.emplace_back(std::bind(&ParallelOpThreadPool::ThreadProc, this));
     }
 
@@ -97,9 +95,9 @@ public:
     }
 
     // Post a task to the thread pool and return a future to wait for its completion
-    std::future<void> PostTask(std::function<void()> task) {
-        std::packaged_task<void()> packagedTask(std::move(task));
-        std::future<void> ret = packagedTask.get_future();
+    std::future<Result> PostTask(std::function<Result()> task) {
+        std::packaged_task<Result()> packagedTask(std::move(task));
+        std::future<Result> ret = packagedTask.get_future();
 
         std::unique_lock<std::mutex> lock(task_queue_mutex);
 
@@ -114,8 +112,6 @@ public:
     }
 
 };
-
-static ParallelOpThreadPool parallel_op_thread_pool;
 
 } // namespace {
 
@@ -189,6 +185,8 @@ GroupElement MultiExponent::get_multiple_single_thread(int start_point, int poin
 }
 
 GroupElement MultiExponent::get_multiple() {
+    static ParallelOpThreadPool<GroupElement> parallel_op_thread_pool;
+
     constexpr int min_points_per_thread = ECMULT_PIPPENGER_THRESHOLD * 3 / 2;
     int points_per_thread = std::max((int)(n_points / std::thread::hardware_concurrency()), min_points_per_thread);
     int n_threads = n_points / points_per_thread;
@@ -198,29 +196,22 @@ GroupElement MultiExponent::get_multiple() {
 
     points_per_thread = n_points / n_threads;
 
-    std::vector<GroupElement> thread_results;
-    thread_results.resize(n_threads);
-
-    boost::this_thread::disable_interruption dnd;
-    std::vector<std::future<void>> parallel_tasks;
+    std::vector<std::shared_future<GroupElement>> parallel_tasks;
 
     parallel_tasks.reserve(n_threads);
 
     int start_point = 0;
     for (int i = 0; i < n_threads; ++i) {
         int point_count = i == n_threads-1 ? n_points - start_point : std::min(points_per_thread, n_points - start_point);
-        parallel_tasks.emplace_back(parallel_op_thread_pool.PostTask([=, &thread_results] {
-            thread_results[i] = get_multiple_single_thread(start_point, point_count);
+        parallel_tasks.emplace_back(parallel_op_thread_pool.PostTask([=] {
+            return get_multiple_single_thread(start_point, point_count);
         }));
         start_point += point_count;
     }
 
-    for (std::future<void> &f: parallel_tasks)
-        f.get();
-
-    GroupElement r = thread_results[0];
-    for (std::size_t n = 1; n < thread_results.size(); ++n)
-        r += thread_results[n];
+    GroupElement r = parallel_tasks[0].get();
+    for (int n = 1; n < n_threads; ++n)
+        r += parallel_tasks[n].get();
 
     return r;
 }
