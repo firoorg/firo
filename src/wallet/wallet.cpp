eda2c55d567f21cf0b0fc8b312e1457e455e976d
riordant <riordant@tcd.ie>
2020-02-03 16:33:13 +0700

    Fix spend-to-mint issue

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -1680,7 +1680,7 @@ void CWalletTx::GetAmounts(list <COutputEntry> &listReceived,
         //   1) they debit from us (sent)
         //   2) the output is to us (received)
 
-        if(!IsZerocoinSpend() || !IsSigmaSpend()){
+        if(!IsSigmaSpend()){
             if (nDebit > 0) {
                 // Don't report 'change' txouts
                 if (IsChange(static_cast<uint32_t>(i)))
@@ -1693,7 +1693,7 @@ void CWalletTx::GetAmounts(list <COutputEntry> &listReceived,
         // In either case, we need to get the destination address
         CTxDestination address;
 
-        if (txout.scriptPubKey.IsZerocoinMint() || txout.scriptPubKey.IsSigmaMint()) {
+        if (txout.scriptPubKey.IsSigmaMint()) {
             address = CNoDestination();
         } else if (!ExtractDestination(txout.scriptPubKey, address) && !txout.scriptPubKey.IsUnspendable()) {
             LogPrintf("CWalletTx::GetAmounts: Unknown transaction type found, txid %s\n",
@@ -1704,7 +1704,7 @@ void CWalletTx::GetAmounts(list <COutputEntry> &listReceived,
         COutputEntry output = {address, txout.nValue, (int) i};
 
         /// If we are debited by the transaction, add the output as a "sent" entry
-        if (nDebit > 0 || ((IsZerocoinSpend() || IsSigmaSpend()) && fromMe)){
+        if (nDebit > 0 || (IsSigmaSpend() && fromMe)){
             listSent.push_back(output);
         }
 
