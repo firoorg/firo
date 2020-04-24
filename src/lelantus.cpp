6d31d6d828be4a1b402c2e69a969e5138f5f7a90
Levon Petrosyan <petrosyan.levon93@gmail.com>
2020-04-24 12:48:29 +0400

    CheckLelantusJoinSplitTransaction almost done

--- src/lelantus.cpp
+++ src/lelantus.cpp
@@ -151,6 +151,122 @@ bool CheckLelantusBlock(CValidationState &state, const CBlock& block) {
     return true;
 }
 
+bool CheckLelantusJoinSplitTransaction(
+        const CTransaction &tx,
+        CValidationState &state,
+        uint256 hashTx,
+        bool isVerifyDB,
+        int nHeight,
+        int nRealHeight,
+        bool isCheckWallet,
+        bool fStatefulSigmaCheck,
+        CLelantusTxInfo* lelantusTxInfo) {
+    std::unordered_set<Scalar, sigma::CScalarHash> txSerials;
+
+    Consensus::Params const & params = ::Params().GetConsensus();
+
+    if(tx.vin.size() != 1 || !tx.vin[0].scriptSig.IsLelantusJoinSplit()) {
+        // mixing lelantus spend input with non-lelantus inputs is prohibited
+        return state.DoS(100, false,
+                         REJECT_MALFORMED,
+                         "CheckLelantusJoinSplitTransaction: can't mix lelantus spend input with other tx types or have more than one spend");
+    }
+
+    const CTxIn &txin = tx.vin[0];
+    std::unique_ptr<lelantus::JoinSplit> joinsplit;
+
+    try {
+        joinsplit = ParseLelantusJoinSplit(txin);
+    }
+    catch (CBadTxIn&) {
+        return state.DoS(100,
+            false,
+            REJECT_MALFORMED,
+            "CheckLelantusJoinSplitTransaction: invalid joinsplit transaction");
+    }
+
+    if (joinsplit->getVersion() != LELANTUS_TX_VERSION_4) {
+        return state.DoS(100,
+                         false,
+                         NSEQUENCE_INCORRECT,
+                         "CTransaction::CheckLelantusJoinSplitTransaction() : Error: incorrect joinsplit transaction verion");
+    }
+
+    uint256 txHashForMetadata;
+
+    // Obtain the hash of the transaction sans the zerocoin part
+    CMutableTransaction txTemp = tx;
+    txTemp.vin[0].scriptSig.clear();
+
+    txHashForMetadata = txTemp.GetHash();
+
+    LogPrintf("CheckLelantusJoinSplitTransaction: tx version=%d, tx metadata hash=%s\n",
+             joinsplit->getVersion(), txHashForMetadata.ToString());
+
+    if (!fStatefulSigmaCheck) {
+        return true;
+    }
+
+    bool passVerify = false;
+    std::unordered_map<uint32_t, std::vector<PublicCoin>> anonymity_sets;
+    std::vector<PublicCoin> Cout;
+    Scalar Vout;
+    Scalar fee;
+//TODO(levon) implement here
+
+    passVerify = joinsplit->Verify(anonymity_sets, Cout, Vout, fee, txHashForMetadata);
+
+    if (passVerify) {
+        const std::vector<Scalar>& serials = joinsplit->getCoinSerialNumbers();
+        // do not check for duplicates in case we've seen exact copy of this tx in this block before
+        if (!(lelantusTxInfo && lelantusTxInfo->zcTransactions.count(hashTx) > 0)) {
+            for(const auto& serial : serials) {
+                if (!CheckLelantusSpendSerial(
+                        state, lelantusTxInfo, serial, nHeight, false)) {
+                    LogPrintf("CheckLelantusJoinSplitTransaction: serial check failed, serial=%s\n", serial);
+                    return false;
+
+                }
+            }
+        }
+
+        // check duplicated serials in same transaction.
+        for(const auto& serial : serials) {
+            if (!txSerials.insert(serial).second) {
+                return state.DoS(100,
+                                 error("CheckLelantusJoinSplitTransaction: two or more spends with same serial in the same transaction"));
+            }
+        }
+
+            if(!isVerifyDB && !isCheckWallet) {
+                if (lelantusTxInfo && !lelantusTxInfo->fInfoIsComplete) {
+                    // add spend information to the index
+                    const std::vector<uint32_t>& ids = joinsplit->getCoinGroupIds();
+                    if(serials.size() != ids.size()) {
+                        return state.DoS(100,
+                                         error("CheckLelantusJoinSplitTransaction: sized of serials and group ids don't match."));
+                    }
+
+                    for(size_t i = 0; i < serials.size(); i++) {
+                        lelantusTxInfo->spentSerials.insert(std::make_pair(serials[i], ids[i]));
+                    }
+                }
+            }
+    }
+    else {
+        LogPrintf("CheckLelantusJoinSplitTransaction: verification failed at block %d\n", nHeight);
+        return false;
+    }
+
+    if(!isVerifyDB && !isCheckWallet) {
+        if (lelantusTxInfo && !lelantusTxInfo->fInfoIsComplete) {
+            lelantusTxInfo->zcTransactions.insert(hashTx);
+        }
+    }
+
+    return true;
+}
+
 bool CheckLelantusMintTransaction(
         const CTxOut &txout,
         CValidationState &state,
@@ -260,8 +376,13 @@ bool CheckLelantusTransaction(
                 "bad-txns-spend-invalid");
         }
 
-//TODO(levon) implement joinsplit checks
-
+        if (!isVerifyDB) {
+            if (!CheckLelantusJoinSplitTransaction(
+                tx, state, hashTx, isVerifyDB, nHeight, realHeight,
+                isCheckWallet, fStatefulSigmaCheck, lelantusTxInfo)) {
+                    return false;
+            }
+        }
     }
 
     return true;
@@ -277,7 +398,16 @@ void RemoveLelantusJoinSplitReferencingBlock(CTxMemPool& pool, CBlockIndex* bloc
             // block removed. If any one is equal, remove txn from mempool.
             for (const CTxIn& txin : tx.vin) {
                 if (txin.IsLelantusJoinSplit()) {
-                    std::unique_ptr<lelantus::JoinSplit> joinsplit = ParseLelantusJoinSplit(txin);
+                    std::unique_ptr<lelantus::JoinSplit> joinsplit;
+
+                    try {
+                        joinsplit = ParseLelantusJoinSplit(txin);
+                    }
+                    catch (const std::ios_base::failure &) {
+                        // nothing
+                        return;
+                    }
+
                     const std::vector<std::pair<uint32_t, uint256>>& coinGroupIdAndBlockHash = joinsplit->getIdAndBlockHashes();
                     for(const auto& idAndHash : coinGroupIdAndBlockHash) {
                         if (idAndHash.second == blockIndex->GetBlockHash()) {
