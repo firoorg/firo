03e4ec3bd14754395e8781717e3585e96a45f440
riordant <riordant@tcd.ie>
2020-05-10 15:10:35 +0700

    Wait for return of REP calls before executing the next

--- src/zmqserver/zmqreplier.cpp
+++ src/zmqserver/zmqreplier.cpp
@@ -119,6 +119,10 @@ bool CZMQAbstractReplier::Wait(){
 
     if (!KEEPALIVE) return false;
 
+    while(!APIIsOpen()){
+        boost::this_thread::sleep_for(boost::chrono::milliseconds(100));
+    }
+
     return true;
 }
 
