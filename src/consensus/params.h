c629746c2855e96f3801d05ea097181ae25426d3
levonpetrosyan93 <45027856+levonpetrosyan93@users.noreply.github.com>
2020-08-20 20:43:18 +0400

    Version bump and Sigma Reactivation (#898)

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -227,6 +227,7 @@ struct Params {
     int nDisableUnpaddedSigmaBlock;
 
     int nStartSigmaBlacklist;
+    int nRestartSigmaWithBlacklistCheck;
 
     // The block number after which old sigma clients are banned.
     int nOldSigmaBanBlock;
