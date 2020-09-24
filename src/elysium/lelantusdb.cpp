42a679fea8ca67fbc09d82bf75670fc9990fda4c
thebevrishot <panu.suk@protonmail.com>
2020-09-24 12:27:48 +0700

    Add coin adding logic

--- src/elysium/lelantusdb.cpp
+++ src/elysium/lelantusdb.cpp
@@ -5,6 +5,12 @@
 #include "convert.h"
 #include "lelantusdb.h"
 
+template<typename T>
+T swapByteOrder(T t) {
+    elysium::swapByteOrder(t);
+    return t;
+}
+
 namespace elysium {
 
 static const char DB_SERIAL             = 0x00;
@@ -125,10 +131,45 @@ bool LelantusDb::WriteMint(
     lelantus::PublicCoin const &pubKey,
     int block)
 {
+    auto coinKey = std::make_tuple(DB_COIN, propertyId, pubKey);
+    if (db.Exists(coinKey)) {
+        return false;
+    }
+
+    if (!db.Write(coinKey, 0)) {
+        throw std::runtime_error("Fail to write coin");
+    }
+
+    // Write sequence in property
+    auto nextSequence = GetNextSequence(DB_COIN_SEQUENCE, propertyId);
+
+    if (!db.Write(std::make_tuple(DB_COIN_SEQUENCE, propertyId, ::swapByteOrder(nextSequence)), std::make_tuple(pubKey, block))) {
+        throw std::runtime_error("Fail to write coin sequence");
+    }
+
+    // Write global sequence
+    auto nextGlobalSequence = GetNextSequence(DB_COIN_GLOBAL_SEQUENCE);
+
+    if (!db.Write(std::make_tuple(DB_COIN_GLOBAL_SEQUENCE, ::swapByteOrder(nextGlobalSequence)), std::make_tuple(propertyId, nextSequence))) {
+        throw std::runtime_error("Fail to write coin global sequence");
+    }
+
+    // add new group if last group is full
+    auto groupSizes = ReadGroupSize();
+
+    size_t coins;
+    auto lastGroup = GetLastGroup(propertyId, coins);
+
+    if (coins > groupSizes.first) {
+        StartNewGroup(propertyId, groupSizes.second);
+    }
+
+    return true;
 }
 
 bool LelantusDb::WriteGroupSize(size_t groupSize, size_t mintAmount)
 {
+    assert(groupSize > mintAmount);
     if (db.Exists(DB_GROUPSIZE)) {
         return false;
     }
@@ -142,8 +183,93 @@ std::pair<size_t, size_t> LelantusDb::ReadGroupSize()
     if (!db.Read(DB_GROUPSIZE, groupSizes)) {
         throw std::runtime_error("Fail to read group size");
     }
+    assert(groupSizes.first > groupSizes.second);
 
     return groupSizes;
+}
+
+int LelantusDb::GetLastGroup(PropertyId id, size_t &coins)
+{
+    auto nextSeq = GetNextSequence(DB_COIN_SEQUENCE, id);
+    coins = nextSeq;
+
+    int group = 0;
+    if (!db.Exists(std::make_tuple(DB_COIN_GROUP, id, group))) {
+        if (!db.Write(std::make_tuple(DB_COIN_GROUP, id, group), (uint64_t)0)) {
+            throw std::runtime_error("Fail to record group id 0");
+        }
+    }
+
+    uint64_t startSequence;
+    while (db.Read(std::make_tuple(DB_COIN_GROUP, id, group + 1), startSequence)) {
+        coins = nextSeq - startSequence;
+        group++;
+    }
+
+    return group;
+}
+
+int LelantusDb::StartNewGroup(PropertyId id, size_t startCoins)
+{
+    auto nextSequence = GetNextSequence(DB_COIN_SEQUENCE, id);
+    if (nextSequence == 0) {
+        if (!db.Write(std::make_tuple(DB_COIN_GROUP, id, 0), 0)) {
+            throw std::runtime_error("Fail to record group id 0");
+        }
+        return 0;
+    }
+
+    // estimate condidate
+    auto firstSeqCandidate = nextSequence - startCoins;
+
+    auto it = NewIterator();
+    it->Seek(std::make_tuple(DB_COIN_SEQUENCE, id, ::swapByteOrder(firstSeqCandidate)));
+
+    if (!it->Valid()) {
+        throw std::runtime_error("Fail to seek candidate");
+    }
+
+    std::pair<lelantus::PublicCoin, int> coin;
+    if (!it->GetValue(coin)) {
+        throw std::runtime_error("Fail to get value");
+    }
+
+    // find the first coin which is in the same block with first candidate
+    int block = coin.second;
+    auto firstSeq = firstSeqCandidate;
+
+    std::tuple<char, PropertyId, uint64_t> key;
+    while (it->Valid() && firstSeq > 0) {
+        if (!it->GetValue(coin)) {
+            throw std::runtime_error("Fail to get value");
+        }
+
+        if (coin.second != block) {
+            break;
+        }
+
+        if (!it->GetKey(key)) {
+            throw std::runtime_error("Fail to get key");
+        }
+
+        if (std::get<1>(key) != id) {
+            throw std::runtime_error("Lelantus database is inconsistent");
+        }
+
+        // record prior sequence which is in the same block
+        firstSeq = ::swapByteOrder(std::get<2>(key));
+
+        it->Prev();
+    }
+
+    size_t coins;
+    auto nextGroup = GetLastGroup(id, coins) + 1;
+
+    if (!db.Write(std::make_tuple(DB_COIN_GROUP, id, nextGroup), firstSeq)) {
+        throw std::runtime_error("Fail to record group id n");
+    }
+
+    return nextGroup;
 }
 
 std::unique_ptr<CDBIterator> LelantusDb::NewIterator()
