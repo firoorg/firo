57d685e629bf0340cab8d520cd6eb6c748989b92
thebevrishot <panu.suk@protonmail.com>
2020-09-27 23:08:55 +0700

    Fix transaction parser

--- src/qt/transactionrecord.cpp
+++ src/qt/transactionrecord.cpp
@@ -8,6 +8,7 @@
 
 #include "base58.h"
 #include "consensus/consensus.h"
+#include "lelantus.h"
 #include "validation.h"
 #include "timedata.h"
 #include "wallet/wallet.h"
@@ -52,8 +53,19 @@ QList<TransactionRecord> TransactionRecord::decomposeTransaction(const CWallet *
             break;
     }
 
-    if (wtx.tx->IsZerocoinSpend() || isAllSigmaSpendFromMe) {
+    bool isAllJoinSplitFromMe;
+    for (const auto& vin : wtx.tx->vin) {
+        isAllJoinSplitFromMe = (wallet->IsMine(vin) & ISMINE_SPENDABLE) && vin.IsLelantusJoinSplit();
+        if (!isAllJoinSplitFromMe)
+            break;
+    }
+
+    if (wtx.tx->IsZerocoinSpend() || isAllSigmaSpendFromMe || isAllJoinSplitFromMe) {
         CAmount nTxFee = nDebit - wtx.tx->GetValueOut();
+        if (isAllJoinSplitFromMe && wtx.tx->vin.size() > 0) {
+            nTxFee = lelantus::ParseLelantusJoinSplit(wtx.tx->vin[0])->getFee();
+        }
+
         bool first = true;
 
         bool isAllToMe = true;
@@ -66,7 +78,7 @@ QList<TransactionRecord> TransactionRecord::decomposeTransaction(const CWallet *
             if (!mine) {
                 isAllToMe = false;
                 break;
-            } else if (!txout.scriptPubKey.IsSigmaMint()) {
+            } else if (!txout.scriptPubKey.IsSigmaMint() && !txout.scriptPubKey.IsLelantusJMint()) {
                 CTxDestination address;
                 ExtractDestination(txout.scriptPubKey, address);
                 if (firstAddress) {
@@ -79,7 +91,7 @@ QList<TransactionRecord> TransactionRecord::decomposeTransaction(const CWallet *
                 involvesWatchAddress = true;
         }
 
-        if(isAllToMe){
+        if (isAllToMe) {
             TransactionRecord sub(hash, nTime);
             sub.involvesWatchAddress = involvesWatchAddress;
             sub.type = TransactionRecord::SpendToSelf;
