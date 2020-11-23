3f416078774bc52a7366b044206074372c2273f3
Admin <petrosyan.levon93@gmail.com>
2020-11-23 10:01:46 +0400

    Fixing cosmetic issue

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -2034,6 +2034,10 @@ UniValue gettransaction(const JSONRPCRequest& request)
     CAmount nFee = (wtx.IsFromMe(filter) ? wtx.tx->GetValueOut() - nDebit : 0);
 
     entry.push_back(Pair("amount", ValueFromAmount(nNet - nFee)));
+
+    if (wtx.tx->vin[0].IsLelantusJoinSplit())
+        nFee = lelantus::ParseLelantusJoinSplit(wtx.tx->vin[0])->getFee();
+
     if (wtx.IsFromMe(filter))
         entry.push_back(Pair("fee", ValueFromAmount(nFee)));
 
