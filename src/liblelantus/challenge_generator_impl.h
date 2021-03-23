6df5e7593b7b570e139c45882566fd686e48ae14
Levon Petrosyan <petrosyan.levon93@gmail.com>
2021-03-23 20:46:20 +0400

    Review comment applied

--- src/liblelantus/challenge_generator_impl.h
+++ src/liblelantus/challenge_generator_impl.h
@@ -14,13 +14,47 @@ template <class Hasher>
 class ChallengeGeneratorImpl : public ChallengeGenerator {
 
 public:
-    ChallengeGeneratorImpl();
-    void add(const GroupElement& group_element);
-    void add(const std::vector<GroupElement>& group_elements);
-    void add(const Scalar& scalar);
-    void add(const std::vector<Scalar>& scalars);
-    void add(const std::vector<unsigned char>& data_);
-    void get_challenge(Scalar& result_out);
+    ChallengeGeneratorImpl() {
+        data.resize(GroupElement::serialize_size);
+        scalar_data.resize(32);
+    }
+
+    void add(const GroupElement& group_element) {
+        group_element.serialize(data.data());
+        hash.Write(data.data(), data.size());
+    }
+
+    void add(const std::vector<GroupElement>& group_elements) {
+        for (size_t i = 0; i < group_elements.size(); ++i) {
+            add(group_elements[i]);
+        }
+    }
+
+    void add(const Scalar& scalar) {
+        scalar.serialize(scalar_data.data());
+        hash.Write(scalar_data.data(), scalar_data.size());
+    }
+
+    void add(const std::vector<Scalar>& scalars) {
+        for (size_t i = 0; i < scalars.size(); ++i) {
+            add(scalars[i]);
+        }
+    }
+
+    void add(const std::vector<unsigned char>& data_) {
+        hash.Write(data_.data(), data_.size());
+    }
+
+    void get_challenge(Scalar& result_out) {
+        unsigned char result_data[CSHA256::OUTPUT_SIZE];
+        do {
+            Hasher temp_hash = hash;
+            hash.Finalize(result_data);
+            hash = temp_hash;
+            result_out = result_data;
+            add(result_out);
+        } while (result_out.isZero() || !result_out.isMember());
+    }
 
 private:
     Hasher hash;
@@ -29,7 +63,5 @@ private:
 };
 
 }// namespace lelantus
-
-#include "challenge_generator_impl.hpp"
 
 #endif //FIRO_LELANTUS_CHALLENGE_GENERATOR_IMPL_H
