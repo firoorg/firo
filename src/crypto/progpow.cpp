0bef649bc795bcccc1f817c413d0fc98eef7624e
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2021-04-17 18:13:07 +0200

    ProgPoW simpler * use hash256_from_bytes instead of hasx string * light hash when mix_hash is known

--- src/crypto/progpow.cpp
+++ src/crypto/progpow.cpp
@@ -12,19 +12,31 @@
 
 #include <sstream>
 
-void progpow_hash(const CBlockHeader& header, uint256& hash, int height)
+std::pair<uint256,uint256> progpow_hash_full(const CBlockHeader& header)
 {
     static ethash::epoch_context_ptr epochContext{nullptr,nullptr};
 
     auto input = header.GetProgPowHeaderHash();
-    ethash::hash256 headerhash{to_hash256(input.GetHex())};
+    auto header_hash {ethash::hash256_from_bytes(input.begin())};
 
-    if (!epochContext || epochContext->epoch_number != ethash::get_epoch_number(height))
+    if (!epochContext || epochContext->epoch_number != ethash::get_epoch_number(header.nHeight))
     {
         epochContext.reset();
-        epochContext = ethash::create_epoch_context(ethash::get_epoch_number(height));
+        epochContext = ethash::create_epoch_context(ethash::get_epoch_number(header.nHeight));
     }
     
-    const auto result = progpow::hash(*epochContext, height, headerhash, header.nNonce64);
-    hash = uint256S(to_hex(result.final_hash));
+    const auto result = progpow::hash(*epochContext, header.nHeight, header_hash, header.nNonce64);
+    return {uint256S(to_hex(result.final_hash), uint256S(to_hex(result.mix_hash))};
+}
+
+uint256 progpow_hash_light(const CBlockHeader& header) 
+{
+    assert(!header.mix_hash.IsNull());
+
+    auto input{header.GetProgPowHeaderHash()};
+    auto header_hash{ethash::hash256_from_bytes(input.begin())};
+    auto seed_hash{progpow::hash_seed(header_hash, header->nNonce64)};
+    auto mix_hash{ethash::hash256_from_bytes(header.mix_hash.begin())};
+    return progpow::hash_final(seed_hash, mix_hash);
+
 }
