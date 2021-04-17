f60371ea7521659d35ccc96d1138f5da7128335d
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2021-04-17 20:08:03 +0200

    CopyRight

--- src/crypto/progpow.cpp
+++ src/crypto/progpow.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2021 barrystyle
+// Copyright (c) 2021 Andrea Lanfranchi
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
