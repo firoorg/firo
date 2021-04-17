70cd5547ed72e4003c7e42ff58e2275cb837e026
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2021-04-17 18:13:07 +0200

    ProgPoW simpler * use hash256_from_bytes instead of hasx string * light hash when mix_hash is known

--- src/crypto/progpow.h
+++ src/crypto/progpow.h
@@ -12,6 +12,10 @@
 class uint256;
 class CBlockHeader;
 
-void progpow_hash(const CBlockHeader& header, uint256& hash, int height);
+/* Performs a full progpow hash (DAG loops implied) provided header already hash nHeight valued */
+std::pair<uint256,uint256> progpow_hash_full(const CBlockHeader& header);
+
+/* Performs a light progpow hash (DAG loops excluded) provided header has mix_hash */
+uint256 progpow_hash_light(const CBlockHeader& header)
 
 #endif // FIRO_PROGPOW_H
