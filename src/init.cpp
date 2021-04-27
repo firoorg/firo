2ec9860e9cf22104842a77e4979b854c9660b4be
a-bezrukov <36845392+a-bezrukov@users.noreply.github.com>
2021-04-27 22:52:23 +0400

    Implemented special handling for -nodebug flag (#1014)
    * Implemented special handling for -nodebug flag

--- src/init.cpp
+++ src/init.cpp
@@ -1207,8 +1207,10 @@ bool AppInitParameterInteraction()
     // Special-case: if -debug=0/-nodebug is set, turn off debugging messages
     if (fDebug) {
         const std::vector<std::string>& categories = mapMultiArgs.at("-debug");
-        if (GetBoolArg("-nodebug", false) || find(categories.begin(), categories.end(), std::string("0")) != categories.end())
+        if (GetBoolArg("-nodebug", false) || find(categories.begin(), categories.end(), std::string("0")) != categories.end()) {
             fDebug = false;
+            fNoDebug = true;
+        }
     }
 
     // Check for -debugnet
