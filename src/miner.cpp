6d3a21b3f20505d0e1bb1d451ae7eb3020c36e50
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2021-05-04 13:16:58 +0200

    Cpp error

--- src/miner.cpp
+++ src/miner.cpp
@@ -1154,7 +1154,6 @@ void static FiroMiner(const CChainParams &chainparams) {
                     boost::this_thread::interruption_point();
 
                     //LogPrintf("*****\nhash   : %s  \ntarget : %s\n", UintToArith256(thash).ToString(), hashTarget.ToString());
-
                     if (UintToArith256(thash) <= hashTarget) {
                         pblock->mix_hash = mix_hash; // Store ProgPoW mix_hash
                         // Found a solution
@@ -1162,7 +1161,7 @@ void static FiroMiner(const CChainParams &chainparams) {
                         SetThreadPriority(THREAD_PRIORITY_NORMAL);
 //                        CheckWork(pblock, *pwallet, reservekey);
                         LogPrintf("FiroMiner:\n");
-                        LogPrintf("proof-of-work found  \n  hash: %s  \ntarget: %s\n", UintToArith256(thash).ToString(), hashTarget.ToString());
+                        LogPrintf("proof-of-work found  \n  hash: %s  \ntarget: %s\n", thash.ToString(), hashTarget.ToString());
                         ProcessBlockFound(pblock, chainparams);
                         SetThreadPriority(THREAD_PRIORITY_LOWEST);
                         coinbaseScript->KeepScript();
