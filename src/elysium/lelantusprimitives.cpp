69063986c45555548fb76aa5a24adc2c3b5300db
sproxet <me@sproxet.dev>
2021-06-03 13:45:43 +0700

    Make sure fee calculation takes into account multiple elysium-lelantus transactions.

--- src/elysium/lelantusprimitives.cpp
+++ src/elysium/lelantusprimitives.cpp
@@ -91,8 +91,15 @@ lelantus::JoinSplit CreateJoinSplit(
     uint256 const &metaData)
 {
     auto params = lelantus::Params::get_default();
-    // 7526 is the size of all Elysium JoinSplit transactions.
-    return {params, coins, anonss, anonymitySetHashes, amount, coinOuts, (uint64_t)payTxFee.GetFee(7526), groupBlockHashs, metaData, LELANTUS_TX_VERSION_4_5};
+    // 4966 is the fixed size part of the transaction. It includes:
+    // + 1 Lelantus input
+    // + 1 Lelantus joinmint output
+    // + 1 reference output
+    // + the Elysium metadata
+    // + the Elysium Lelantus outputs
+    //
+    // Then for every Elysium Lelantus input, there is an additional 2560 bytes of data.
+    return {params, coins, anonss, anonymitySetHashes, amount, coinOuts, (uint64_t)payTxFee.GetFee(4966 + coins.size() * 2560), groupBlockHashs, metaData, LELANTUS_TX_VERSION_4_5};
 }
 
 } // namespace elysium
