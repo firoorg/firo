baf0752d41fefc1e3e8741828bda14616f7830bc
Peter Shugalev <peter@shugalev.com>
2021-07-16 16:03:18 +0300

    Merged PR #1036

--- src/hdmint/tracker.cpp
+++ src/hdmint/tracker.cpp
@@ -215,7 +215,7 @@ std::vector<uint256> CHDMintTracker::GetSerialHashes()
  * @param hashPubcoin mint pubcoin hash
  * @return success
  */
-bool CHDMintTracker::HasPubcoinHash(const uint256& hashPubcoin, CWalletDB& walletdb) const
+bool CHDMintTracker::HasSigmaPubcoinHash(const uint256& hashPubcoin, CWalletDB& walletdb) const
 {
     for (auto const & it : mapSerialHashes) {
         CMintMeta meta = it.second;
@@ -223,6 +223,11 @@ bool CHDMintTracker::HasPubcoinHash(const uint256& hashPubcoin, CWalletDB& walle
             return true;
     }
 
+    return false;
+}
+
+bool CHDMintTracker::HasLelantusPubcoinHash(const uint256& hashPubcoin, CWalletDB& walletdb) const
+{
     for (auto const & it : mapLelantusSerialHashes) {
         CLelantusMintMeta meta = it.second;
         uint256 reducedHash;
