762e236bed0f1921d34f5587c9c2cf2aa9c88629
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2021-07-17 17:25:07 +0200

    Amend include

--- src/crypto/progpow.cpp
+++ src/crypto/progpow.cpp
@@ -2,7 +2,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include <crypto/progpow.h>
+#include "progpow.h"
 
 #include <chainparams.h>
 #include <crypto/progpow/helpers.hpp>
