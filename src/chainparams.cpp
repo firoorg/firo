1261c84250840012db53080a8f0d7727a4f084ea
Peter Shugalev <peter@shugalev.com>
2021-09-10 16:47:16 +0300

    Seeds for devnet

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -843,7 +843,7 @@ public:
         base58Prefixes[SECRET_KEY] = std::vector < unsigned char > (1, 186);
         base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x35)(0x87)(0xD0).convert_to_container < std::vector < unsigned char > > ();
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x35)(0x83)(0x95).convert_to_container < std::vector < unsigned char > > ();
-        vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_test, pnSeed6_test + ARRAYLEN(pnSeed6_test));
+        vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_dev, pnSeed6_dev + ARRAYLEN(pnSeed6_dev));
 
         fMiningRequiresPeers = true;
         fDefaultConsistencyChecks = false;
