8396d2947331fc13d6b565deb20d9ec96bd56fcc
fanquake <fanquake@gmail.com>
2022-01-26 08:57:51 +0800

    guix: use autoconf 2.71
    This allows for building with newer targets, like arm64-apple-darwin, due to
    having a newer bundled config.guess and config.sub.

--- contrib/guix/manifest.scm
+++ contrib/guix/manifest.scm
@@ -629,7 +629,7 @@ inspecting signatures in Mach-O binaries.")
         ;; Build tools
         gnu-make
         libtool
-        autoconf
+        autoconf-2.71
         automake
         pkg-config
         bison
