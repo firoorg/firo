5ae45722d534794b5fcd9c7a9df0be11a514d43b
firstcryptoman <firstcryptoman@gmail.com>
2022-03-14 12:49:13 +0400

    Fixing joinsplit fee estimation isueue

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -3473,18 +3473,10 @@ UniValue joinsplit(const JSONRPCRequest& request) {
 
 
     UniValue sendTo = request.params[0].get_obj();
-    UniValue mintAmounts;
-    if(request.params.size() >= 3) {
-        try {
-                mintAmounts = request.params[2].get_obj();
-        } catch (std::runtime_error const &) {
-            //may be empty
-        }
-    }
 
     std::unordered_set<std::string> subtractFeeFromAmountSet;
     UniValue subtractFeeFromAmount(UniValue::VARR);
-    if (request.params.size() > 2) {
+    if (request.params.size() > 1) {
         try {
             subtractFeeFromAmount = request.params[1].get_array();
         }  catch (std::runtime_error const &) {
@@ -3492,6 +3484,15 @@ UniValue joinsplit(const JSONRPCRequest& request) {
         }
         for (int i = subtractFeeFromAmount.size(); i--;) {
             subtractFeeFromAmountSet.insert(subtractFeeFromAmount[i].get_str());
+        }
+    }
+
+    UniValue mintAmounts;
+    if(request.params.size() > 2) {
+        try {
+                mintAmounts = request.params[2].get_obj();
+        } catch (std::runtime_error const &) {
+            //may be empty
         }
     }
 
