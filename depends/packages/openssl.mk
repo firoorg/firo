21223affe8c333e17f1fb01d20343086108305bb
psolstice <peter@shugalev.com>
2022-04-23 19:02:55 +0400

    Fix for darwin guix build (#1165)
    * guix: ignore additioanl failing certvalidator test
    
    ======================================================================
    ERROR: test_revocation_mode_soft (tests.test_validate.ValidateTests)
    ----------------------------------------------------------------------
    Traceback (most recent call last):
      File "/tmp/guix-build-python-certvalidator-0.1-1.e5bdb4b.drv-0/source/tests/test_validate.py", line 85, in test_revocation_mode_soft
        validate_path(context, path)
      File "/tmp/guix-build-python-certvalidator-0.1-1.e5bdb4b.drv-0/source/tests/../certvalidator/validate.py", line 50, in validate_path
        return _validate_path(validation_context, path)
      File "/tmp/guix-build-python-certvalidator-0.1-1.e5bdb4b.drv-0/source/tests/../certvalidator/validate.py", line 358, in _validate_path
        raise PathValidationError(pretty_message(
    certvalidator.errors.PathValidationError: The path could not be validated because the end-entity certificate expired 2022-01-14 12:00:00Z
    
    Github-Pull: #24057
    Rebased-From: 85885919656a3f606f3d7f208378aabe95f3f62d
    
    * build: use macOS 11 SDK (Xcode 12.2)
    
    This should be sufficient to support building for Apple ARM when
    cross-compiling.
    
    * contrib: support arm64 darwin in security checks
    
    * guix: use autoconf 2.71
    
    This allows for building with newer targets, like arm64-apple-darwin, due to
    having a newer bundled config.guess and config.sub.
    
    * guix: add arm64-apple-darwin triplet
    
    * Fix for arm64-darwin compilation
    
    Co-authored-by: fanquake <fanquake@gmail.com>

--- depends/packages/openssl.mk
+++ depends/packages/openssl.mk
@@ -46,7 +46,7 @@ $(package)_config_opts_powerpc_linux=linux-generic32
 $(package)_config_opts_x86_64_darwin=darwin64-x86_64-cc
 $(package)_config_opts_x86_64_mingw32=mingw64
 $(package)_config_opts_i686_mingw32=mingw
-$(package)_config_opts_arm_darwin=darwin64-arm64-cc
+$(package)_config_opts_aarch64_darwin=darwin64-arm64-cc
 endef
 
 define $(package)_preprocess_cmds
