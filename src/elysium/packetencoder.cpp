ebc02834b7ae5fa09b16a4024afe80efcd87b294
sproxet <me@sproxet.dev>
2022-08-02 15:23:36 +0700

    Speed up Elysium's DeterminePacketClass().

--- src/elysium/packetencoder.cpp
+++ src/elysium/packetencoder.cpp
@@ -37,35 +37,8 @@ const CBitcoinAddress& GetSystemAddress()
 
 boost::optional<PacketClass> DeterminePacketClass(const CTransaction& tx, int height)
 {
-    // Inspect all outputs.
-    bool hasOpReturn = false;
-
     for (auto& output : tx.vout) {
-        txnouttype type;
-
-        if (!GetOutputType(output.scriptPubKey, type)) {
-            continue;
-        }
-
-        if (!IsAllowedOutputType(type, height)) {
-            continue;
-        }
-
-        if (type == TX_NULL_DATA) {
-            // Check if the first push is prefixed with magic bytes.
-            std::vector<std::vector<unsigned char>> pushes;
-
-            GetPushedValues(output.scriptPubKey, std::back_inserter(pushes));
-
-            if (!pushes.empty() && pushes[0].size() >= magic.size() && std::equal(magic.begin(), magic.end(), pushes[0].begin())) {
-                hasOpReturn = true;
-            }
-        }
-    }
-
-    // Determine packet class based on inspection result.
-    if (hasOpReturn) {
-        return PacketClass::C;
+        if (output.scriptPubKey.IsElysium()) return PacketClass::C;
     }
 
     return boost::none;
