0423acb1ee04ace93658c12815d569736e909af5
sproxet <me@sproxet.dev>
2022-09-14 18:04:37 +0700

    Attempt to fix compilation in Guix.

---
+++ src/client-api/bigint.cpp
@@ -0,0 +1,15 @@
+#include "univalue.h"
+
+// The client will parse objects created by this function as JS bigints.
+UniValue BigInt(std::string s) {
+    UniValue r = UniValue::VOBJ;
+    r.pushKV("bigint", s);
+    return r;
+}
+
+UniValue BigInt(uint64_t n) {
+    std::ostringstream s;
+    s << n;
+
+    return BigInt(s.str());
+}
--- src/client-api/bigint.h
+++ src/client-api/bigint.h
@@ -1,20 +1,7 @@
 #ifndef ELYSIUM_CPP_BIGINT_H
 #define ELYSIUM_CPP_BIGINT_H
 
-#include "univalue.h"
-
-// The client will parse objects created by this function as JS bigints.
-UniValue BigInt(std::string s) {
-    UniValue r = UniValue::VOBJ;
-    r.pushKV("bigint", s);
-    return r;
-}
-
-UniValue BigInt(uint64_t n) {
-    std::ostringstream s;
-    s << n;
-
-    return BigInt(s.str());
-}
+UniValue BigInt(std::string s);
+UniValue BigInt(uint64_t n);
 
 #endif //ELYSIUM_CPP_BIGINT_H
