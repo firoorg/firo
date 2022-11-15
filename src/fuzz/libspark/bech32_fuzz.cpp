cc86a41c4142826b844784510f4b0a270a0606b2
Mikerah <mikerah@hashcloak.com>
2022-11-15 01:11:57 -0500

    Integrated FuzzedDataProvider.h in bech32_fuzz.cpp

--- src/fuzz/libspark/bech32_fuzz.cpp
+++ src/fuzz/libspark/bech32_fuzz.cpp
@@ -1,17 +1,27 @@
-#include "../libspark/bech32.h"
+#include "../../libspark/bech32.h"
+#include "../FuzzedDataProvider.h"
 #include <stdint.h>
+#include <cassert>
 
 extern "C" int LLVMFuzzerTestOneInput(uint8_t *buf, size_t len) {
-    std::string test_string = std::string((char *) buf);
-    std::vector<uint8_t> test_vec;
-    for(int i=0; i < len; i++) {
-        test_vec.push_back(buf[i]);
-    }
-    bech32::Encoding test_enc = static_cast<bech32::Encoding>(rand() % 3);
-    std::string test_string_res = std::string((char *) buf);
+    FuzzedDataProvider fuzzed_data(buf, len);
+
+    std::string test_string = fuzzed_data.ConsumeBytesAsString(len);
+    std::vector<uint8_t> test_vec = fuzzed_data.ConsumeBytes<uint8_t>(len);
+    bech32::Encoding test_enc = fuzzed_data.ConsumeEnum<bech32::Encoding>();
+    std::string test_string_res;
     test_string_res = bech32::encode(test_string, test_vec, test_enc);
-    const std::string& test_const_string = test_string;
-    bech32::decode(test_const_string);
-    bech32::convertbits(test_vec, test_vec, len, len, (bool)(rand() % 2));
+    bech32::DecodeResult dr;
+    dr = bech32::decode(test_string_res);
+    assert(dr.hrp == test_string);
+    assert(dr.encoding == test_enc);
+    assert(dr.data == test_vec);
+
+    std::vector<uint8_t> test_vec1 = fuzzed_data.ConsumeBytes<uint8_t>(len);
+    std::vector<uint8_t> test_vec2 = fuzzed_data.ConsumeBytes<uint8_t>(len);
+    int test_frombits = fuzzed_data.ConsumeIntegral<int>();
+    int test_to_bits = fuzzed_data.ConsumeIntegral<int>();
+    bool test_pad = fuzzed_data.ConsumeBool();
+    bech32::convertbits(test_vec1, test_vec2, test_frombits, test_to_bits, test_pad);
     return 0;
 }
