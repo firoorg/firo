3ea8e4a6edc64b322c3e57f32b060f14bb0739d5
Mikerah <mikerah@hashcloak.com>
2022-11-21 00:10:47 -0500

    Added vector versions of GetGroupElement and GetScalar in fuzzing_utilities.cpp

--- src/fuzz/fuzzing_utilities.cpp
+++ src/fuzz/fuzzing_utilities.cpp
@@ -19,3 +19,23 @@ secp_primitives::Scalar FuzzedSecp256k1Object::GetScalar() {
     return s;
 
 }
+
+std::vector<secp_primitives::GroupElement> FuzzedSecp256k1Object::GetGroupElements(int len) {
+    std::vector<secp_primitives::GroupElement> ge_vec;
+    ge_vec.resize(len);
+    for (int i = 0; i <= len; i++) {
+        ge_vec.push_back(GetGroupElement());
+    }
+
+    return ge_vec;
+}
+
+std::vector<secp_primitives::Scalar> FuzzedSecp256k1Object::GetScalars(int len) {
+    std::vector<secp_primitives::Scalar> scalar_vec;
+    scalar_vec.resize(len);
+    for (int i = 0; i <= len; i++) {
+        scalar_vec.push_back(GetScalar());
+    }
+
+    return scalar_vec;
+}
