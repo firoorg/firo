9b9a517126c29b498d6b55b5f61c6b6fa166cd25
levonpetrosyan93 <petrosyan.levon93@gmail.com>
2022-11-24 06:21:58 +0400

    Addedd aead_fuzz.cpp and other utilities
    Adding check to stop lelantus on consensus level
    
    Added fuzz folder for persistent fuzzing libspark fuzzing
    
    bech32 fuzzing harness for honggfuzz added
    
    Added f4grumble fuzzing harness
    
    Revert "missing semicolon"
    
    This reverts commit 330cff61bd2ed9cefa0720a5b2ce5b356bfff777.
    
    Fuzzing directory cleanup
    
    Added FuzzedDataProvider.h
    
    Added fuzzing utilities for group elements and scalars
    
    Added fuzzing for aead.cpp

---
+++ src/fuzz/fuzzing_utilities.h
@@ -0,0 +1,14 @@
+#include "FuzzedDataProvider.h"
+#include "../secp256k1/include/Scalar.h"
+#include "../secp256k1/include/GroupElement.h"
+
+class FuzzedSecp256k1Object {
+    public: 
+        FuzzedSecp256k1Object(FuzzedDataProvider *fdp);
+        ~FuzzedSecp256k1Object();
+
+        FuzzedDataProvider *fdp;
+
+        secp_primitives::GroupElement GetGroupElement();
+        secp_primitives::Scalar GetScalar();
+};
