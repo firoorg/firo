99a357aee25ddd3458b77622a147e49b28a9f364
levonpetrosyan93 <petrosyan.levon93@gmail.com>
2022-12-14 05:03:19 +0400

    Transaction rejected into mempool issue fixed

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -4258,7 +4258,7 @@ bool CWallet::CreateTransaction(const std::vector<CRecipient>& vecSend, CWalletT
                     nBytes += GetSizeOfCompactSize(nExtraPayloadSize) + nExtraPayloadSize;
                 }
 
-                if (nBytes > MAX_STANDARD_TX_SIZE) {
+                if (GetTransactionWeight(txNew) >= MAX_STANDARD_TX_WEIGHT) {
                     // Do not create oversized transactions (bad-txns-oversize).
                     strFailReason = _("Transaction too large");
                     return false;
