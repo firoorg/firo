11250da6579236d7d6efe2c6c760cd67ad5ec429
levonpetrosyan93 <petrosyan.levon93@gmail.com>
2023-05-16 03:24:45 +0400

    Failing unitests fixed

--- src/primitives/transaction.cpp
+++ src/primitives/transaction.cpp
@@ -225,7 +225,7 @@ bool CTransaction::IsSparkSpend() const
 bool CTransaction::IsSparkMint() const
 {
     for (const CTxOut &txout: vout) {
-        if (txout.scriptPubKey.IsSparkMint()  || txout.scriptPubKey.IsSparkSMint())
+        if (txout.scriptPubKey.IsSparkMint())
             return true;
     }
     return false;
