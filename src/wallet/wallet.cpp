2952760058b95b09a545d7e2d5269826983171fa
levonpetrosyan93 <petrosyan.levon93@gmail.com>
2023-06-21 15:13:33 +0400

    Recognize incoming spark mint

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -1887,6 +1887,27 @@ CAmount CWallet::GetChange(const uint256& tx, const CTxOut &txout) const
 
 bool CWallet::IsMine(const CTransaction& tx) const
 {
+    if (tx.IsSparkTransaction()) {
+        if (!sparkWallet)
+            false;
+        std::vector<unsigned char> serialContext = spark::getSerialContext(tx);
+        for (const auto& txout : tx.vout) {
+            if (txout.scriptPubKey.IsSparkMint() || txout.scriptPubKey.IsSparkSMint()) {
+                spark::Coin coin(spark::Params::get_default());
+                try {
+                    spark::ParseSparkMintCoin(txout.scriptPubKey, coin);
+                } catch (std::invalid_argument &) {
+                    return ISMINE_NO;
+                }
+
+                coin.setSerialContext(serialContext);
+                if(sparkWallet->isMine(coin))
+                    return true;
+            }
+        }
+        return false;
+    }
+
     BOOST_FOREACH(const CTxOut& txout, tx.vout)
         if (IsMine(txout))
             return true;
