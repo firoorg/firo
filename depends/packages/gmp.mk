0aea5a1b4295e68fa0c645678517b8936792c205
Peter Shugalev <peter@shugalev.com>
2023-07-10 13:01:38 +0200

    Fix crash for Apple's recent compiler on ARM64

--- depends/packages/gmp.mk
+++ depends/packages/gmp.mk
@@ -3,6 +3,7 @@ $(package)_version=6.2.1
 $(package)_download_path=https://gmplib.org/download/gmp
 $(package)_file_name=gmp-$($(package)_version).tar.bz2
 $(package)_sha256_hash=eae9326beb4158c386e39a356818031bd28f3124cf915f8c5b1dc4c7a36b4d7c
+$(package)_patches=applem1.patch
 
 define $(package)_set_vars
 $(package)_config_opts+=--enable-cxx --enable-fat --with-pic --disable-shared
@@ -12,6 +13,10 @@ endef
 
 define $(package)_config_cmds
   $($(package)_autoconf)
+endef
+
+define $(package)_preprocess_cmds
+  patch -p1 <$($(package)_patch_dir)/applem1.patch
 endef
 
 define $(package)_build_cmds
