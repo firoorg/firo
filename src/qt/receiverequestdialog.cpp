6907e3edf4aa4af57de4ea82e196c95daca2a1b2
levoncrypto1994@gmail.com <levoncrypto1994@gmail.com>
2023-08-11 14:43:08 +0400

    Fix receive detail tab size issue

--- src/qt/receiverequestdialog.cpp
+++ src/qt/receiverequestdialog.cpp
@@ -130,6 +130,7 @@ void ReceiveRequestDialog::update()
 {
     if(!model)
         return;
+    resize(width(), 700);
     QString target = info.label;
     if(target.isEmpty())
         target = info.address;
