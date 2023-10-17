c2a5aeec761b8886a699130296daeb19985ca337
JusAeng <justo_play@hotmail.com>
2023-10-17 08:20:48 +0000

    implement balance proof

---
+++ src/libspats/balance.cpp
@@ -0,0 +1,58 @@
+#include "balance.h"
+#include "transcript.h"
+
+namespace spats{
+
+Balance::Balance(const GroupElement& E_, const GroupElement& F_, const GroupElement& H_):
+    E(E_),F(F_),H(H_){
+}
+
+Scalar Balance::challenge(
+        const GroupElement& C,
+        const GroupElement& A) {
+    Transcript transcript(LABEL_TRANSCRIPT_BALANCE);
+    transcript.add("E", E);
+    transcript.add("F", F);
+    transcript.add("H", H);
+    transcript.add("C", C);
+    transcript.add("A", A);
+
+    return transcript.challenge("c");
+}
+
+void Balance::prove(const GroupElement& C, const Scalar& w, const Scalar& x, const Scalar& z, BalanceProof& proof) {
+    Scalar rw;
+    Scalar rx;
+    Scalar rz;
+    rw.randomize();
+    rx.randomize();
+    rz.randomize();
+    proof.A = E*rw + F*rx + H*rz;
+
+    const Scalar c = challenge(C, proof.A);
+
+    proof.tw = rw + c*w;
+    proof.tx = rx + c*x;
+    proof.tz = rz + c*z;
+
+}
+
+bool Balance::verify(const GroupElement& C, const BalanceProof& proof) {
+    const GroupElement check1 = E*proof.tw + F*proof.tx + H*proof.tz;
+
+    const Scalar c = challenge(C, proof.A);
+    const GroupElement check2 = proof.A + C*c;
+
+    std::cout <<"check1: "<< check1 << std::endl;
+    std::cout <<"check2: "<< check2 << std::endl;
+
+    if (check1 == check2){
+        std::cout <<"It work!!"<< std::endl;
+    }
+
+
+    return true;
+}
+
+
+}
