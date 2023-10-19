fd13db67f900e222b9baa73b64d681c1df0067f3
JusAeng <justo_play@hotmail.com>
2023-10-19 11:24:29 +0000

    add test case

--- src/libspats/test/balance_test.cpp
+++ src/libspats/test/balance_test.cpp
@@ -1,4 +1,6 @@
 #include "../balance.h"
+#include "../../streams.h"
+#include "../../version.h"
 
 #include "../../test/test_bitcoin.h"
 #include <boost/test/unit_test.hpp>
@@ -9,13 +11,15 @@ using namespace secp_primitives;
 
 BOOST_FIXTURE_TEST_SUITE(spats_balance_proof_tests, BasicTestingSetup)
 
-BOOST_AUTO_TEST_CASE(verify_proof)
+BOOST_AUTO_TEST_CASE(serialization)
 {
-    // Parameters
-    const Params* params;
-    params = Params::get_default();
-    
-    Balance bal(params->get_E(),params->get_F(),params->get_H());
+    GroupElement E;
+    E.randomize();
+    GroupElement F;
+    F.randomize();
+    GroupElement H;
+    H.randomize();
+
     Scalar w;
     Scalar x;
     Scalar z;
@@ -23,12 +27,94 @@ BOOST_AUTO_TEST_CASE(verify_proof)
     x.randomize();
     z.randomize();
 
-    BalanceProof bp = BalanceProof();
+    BalanceProof proof;
+    GroupElement C = E*w+F*x+H*z;
 
-    bal.prove(params->get_E()*w+params->get_F()*x+params->get_H()*z,w,x,z,bp);
+    Balance bal(E,F,H);
+    bal.prove(C, w,x,z, proof);
+
+    CDataStream serialized(SER_NETWORK, PROTOCOL_VERSION);
+    serialized << proof;
+
+    BalanceProof deserialized;
+    serialized >> deserialized;
+
+    BOOST_CHECK(proof.A == deserialized.A);
+    BOOST_CHECK(proof.tw == deserialized.tw);
+    BOOST_CHECK(proof.tx == deserialized.tx);
+    BOOST_CHECK(proof.tz == deserialized.tz);
+}
+
+BOOST_AUTO_TEST_CASE(completeness)
+{
+    GroupElement E;
+    E.randomize();
+    GroupElement F;
+    F.randomize();
+    GroupElement H;
+    H.randomize();
+    
+    Scalar w;
+    Scalar x;
+    Scalar z;
+    w.randomize();
+    x.randomize();
+    z.randomize();
+
+    BalanceProof proof;
+    GroupElement C = E*w+F*x+H*z;
+
+    Balance bal(E,F,H);
+    bal.prove(C,w,x,z,proof);
 
     // Verify
-    BOOST_CHECK(bal.verify(params->get_E()*w+params->get_F()*x+params->get_H()*z,bp));
+    BOOST_CHECK(bal.verify(C,proof));
+}
+
+BOOST_AUTO_TEST_CASE(bad_proofs)
+{
+    GroupElement E;
+    E.randomize();
+    GroupElement F;
+    F.randomize();
+    GroupElement H;
+    H.randomize();
+
+    Scalar w;
+    Scalar x;
+    Scalar z;
+    w.randomize();
+    x.randomize();
+    z.randomize();
+
+    BalanceProof proof;
+    GroupElement C = E*w+F*x+H*z;
+
+    Balance bal(E,F,H);
+    bal.prove(C,w,x,z,proof);
+
+    // Bad C
+    GroupElement evil_C;
+    evil_C.randomize();
+    BOOST_CHECK(!(bal.verify(evil_C, proof)));
+
+    // Bad A
+    BalanceProof evil_proof = proof;
+    evil_proof.A.randomize();
+    BOOST_CHECK(!(bal.verify(C, evil_proof)));
+
+    // Bad tw
+    evil_proof = proof;
+    evil_proof.tw.randomize();
+    BOOST_CHECK(!(bal.verify(C, evil_proof)));
+    // Bad tx
+    evil_proof = proof;
+    evil_proof.tx.randomize();
+    BOOST_CHECK(!(bal.verify(C, evil_proof)));
+    // Bad tz
+    evil_proof = proof;
+    evil_proof.tz.randomize();
+    BOOST_CHECK(!(bal.verify(C, evil_proof)));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
