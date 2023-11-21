16d41e0a6e864b2ec4b02525b03d06c8cde27a69
Peter Shugalev <peter@shugalev.com>
2023-11-21 19:09:14 +0100

    Allow OP_EXCHANGEADDR only in specific situations

--- src/script/interpreter.cpp
+++ src/script/interpreter.cpp
@@ -268,6 +268,7 @@ bool EvalScript(std::vector<std::vector<unsigned char> >& stack, const CScript& 
 
     try
     {
+        bool fFirstOpCode = true;
         while (pc < pend)
         {
             bool fExec = !count(vfExec.begin(), vfExec.end(), false);
@@ -343,7 +344,14 @@ bool EvalScript(std::vector<std::vector<unsigned char> >& stack, const CScript& 
                 // Control
                 //
                 case OP_NOP:
+                    break;
+
                 case OP_EXCHANGEADDR:
+                    // allow OP_EXCHANGEADDR only at the beginning of the script
+                    if (!fFirstOpCode)
+                        return set_error(serror, SCRIPT_ERR_BAD_OPCODE);
+                    // otherwise NOOP
+                    printf("OP_EXCHANGEADDR\n");
                     break;
 
                 case OP_CHECKLOCKTIMEVERIFY:
@@ -1031,6 +1039,8 @@ bool EvalScript(std::vector<std::vector<unsigned char> >& stack, const CScript& 
             // Size limits
             if (stack.size() + altstack.size() > 1000)
                 return set_error(serror, SCRIPT_ERR_STACK_SIZE);
+
+            fFirstOpCode = false;
         }
     }
     catch (...)
