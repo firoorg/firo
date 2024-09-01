0fdb02fb9e5f1706e44d4875eb838560c6f87c93
levonpetrosyan93 <petrosyan.levon93@gmail.com>
2024-09-02 03:24:35 +0400

    Extra payload size limit increased

--- src/validation.cpp
+++ src/validation.cpp
@@ -662,7 +662,7 @@ bool CheckTransaction(const CTransaction &tx, CValidationState &state, bool fChe
     // Size limits (this doesn't take the witness into account, as that hasn't been checked for malleability)
     if (::GetSerializeSize(tx, SER_NETWORK, PROTOCOL_VERSION | SERIALIZE_TRANSACTION_NO_WITNESS) > MAX_BLOCK_BASE_SIZE)
         return state.DoS(100, false, REJECT_INVALID, "bad-txns-oversize");
-    if (tx.vExtraPayload.size() > MAX_TX_EXTRA_PAYLOAD)
+    if ((tx.vExtraPayload.size() > MAX_TX_EXTRA_PAYLOAD && nHeight < ::Params().GetConsensus().stage4StartBlock) || tx.vExtraPayload.size() > NEW_MAX_TX_EXTRA_PAYLOAD)
         return state.DoS(100, false, REJECT_INVALID, "bad-txns-payload-oversize");
 
     // Check for negative or overflow output values
