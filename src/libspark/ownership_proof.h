12ac0b3c343cefd51c58fa790f18fe8b6ebdf3dd
levonpetrosyan93 <petrosyan.levon93@gmail.com>
2024-10-06 04:55:10 +0400

    Missing files added

---
+++ src/libspark/ownership_proof.h
@@ -0,0 +1,31 @@
+#ifndef FIRO_LIBSPARK_OWNERSHIP_PROOF_H
+#define FIRO_LIBSPARK_OWNERSHIP_PROOF_H
+
+#include "params.h"
+
+namespace spark {
+
+class OwnershipProof{
+public:
+    inline std::size_t memoryRequired() const {
+        return Scalar::memoryRequired() * 3 + GroupElement::memoryRequired();
+    }
+
+    ADD_SERIALIZE_METHODS;
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action) {
+        READWRITE(A);
+        READWRITE(t1);
+        READWRITE(t2);
+        READWRITE(t3);
+    }
+
+public:
+    GroupElement A;
+    Scalar t1;
+    Scalar t2;
+    Scalar t3;
+};
+}
+
+#endif
