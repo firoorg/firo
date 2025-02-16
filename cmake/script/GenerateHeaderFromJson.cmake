15b408810ee0dc6cc3a7ad9de1b169799b3c8288
AlefLm <aleflm@proton.me>
2025-02-16 22:47:00 +0000

    CMake: add tests (test_bitcoin) compilation support.

--- cmake/script/GenerateHeaderFromJson.cmake
+++ cmake/script/GenerateHeaderFromJson.cmake
@@ -2,21 +2,33 @@
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or https://opensource.org/license/mit/.
 
-cmake_path(GET JSON_SOURCE_PATH STEM json_source_basename)
+message(STATUS "Generating header ${HEADER_PATH} from ${JSON_SOURCE_PATH}")
 
-file(READ ${JSON_SOURCE_PATH} hex_content HEX)
-string(REGEX REPLACE "................" "\\0\n" formatted_bytes "${hex_content}")
-string(REGEX REPLACE "[^\n][^\n]" "0x\\0, " formatted_bytes "${formatted_bytes}")
+# Generate a header file from a JSON file containing a hex dump
+function(generate_json_header JSON_SOURCE_PATH HEADER_PATH)
+    # Use get_filename_component instead of cmake_path
+    get_filename_component(json_source_basename "${JSON_SOURCE_PATH}" NAME_WE)
 
-set(header_content
-"#include <string_view>
+    # Create directory
+    get_filename_component(header_dir "${HEADER_PATH}" DIRECTORY)
+    file(MAKE_DIRECTORY "${header_dir}")
+    
+    # Read and convert to hex
+    file(READ ${JSON_SOURCE_PATH} hex_content HEX)
+    string(REGEX REPLACE ".." "0x\\0, " formatted_bytes "${hex_content}")
 
-namespace json_tests {
-inline constexpr char detail_${json_source_basename}_bytes[] {
-${formatted_bytes}
-};
+    # Generate content
+    set(header_content
+        "namespace json_tests{
+            static unsigned const char ${json_source_basename}[] = { 
+                ${formatted_bytes}
+            };
+         };"
+    )
+    
+    # Atomic write using temporary file
+    file(WRITE "${HEADER_PATH}.new" "${header_content}")
+    file(RENAME "${HEADER_PATH}.new" "${HEADER_PATH}")
+endfunction()
 
-inline constexpr std::string_view ${json_source_basename}{std::begin(detail_${json_source_basename}_bytes), std::end(detail_${json_source_basename}_bytes)};
-}
-")
-file(WRITE ${HEADER_PATH} "${header_content}")
+generate_json_header(${JSON_SOURCE_PATH} ${HEADER_PATH})
