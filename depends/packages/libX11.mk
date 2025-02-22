c99364274efb2c538c8dbef85d8bf0a139f45da1
AlefLm <aleflm@proton.me>
2025-02-22 08:19:28 +0000

    Qt: CMake GUI compilation support (Ubuntu)

--- depends/packages/libX11.mk
+++ depends/packages/libX11.mk
@@ -6,7 +6,7 @@ $(package)_sha256_hash=2aa027e837231d2eeea90f3a4afe19948a6eb4c8b2bec0241eba7dbc8
 $(package)_dependencies=libxcb xtrans xextproto xproto
 
 define $(package)_set_vars
-$(package)_config_opts=--disable-xkb --disable-static
+$(package)_config_opts=--disable-xkb
 $(package)_config_opts_linux=--with-pic
 endef
 
