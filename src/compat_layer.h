4b6ded333ad3bc77555dc1d8a299cd7895f11f70
AlefLM <aleflm@proton.me>
2025-05-11 08:42:49 +0000

    Fix Wdeprecated-declarations by using snprintf instead of sprintf.

--- src/compat_layer.h
+++ src/compat_layer.h
@@ -22,17 +22,18 @@
     #endif
 #endif
 
-#if defined(__cplusplus) && (__cplusplus >= 201703L) \
-    && defined(__has_attribute) && __has_attribute(fallthrough)
-  #define FIRO_FALLTHROUGH [[fallthrough]]
-#elif !defined(__clang__) && defined(__has_attribute)
- #if __has_attribute(gnu::fallthrough)
-  #define FIRO_FALLTHROUGH [[gnu::fallthrough]]
- #endif
-#elif defined(__has_attribute) && __has_attribute(fallthrough)
-  #define FIRO_FALLTHROUGH __attribute__((fallthrough))
-#elif defined(__GNUC__) && (__GNUC__ >= 7) && !defined(__clang__)
-  #define FIRO_FALLTHROUGH __attribute__((fallthrough))
+#if defined(__cplusplus) && (__cplusplus >= 201703L)
+    #if defined(__has_attribute) && __has_attribute(fallthrough)
+        #define FIRO_FALLTHROUGH [[fallthrough]]
+    #elif !defined(__clang__) && defined(__has_attribute)
+        #if __has_attribute(gnu::fallthrough)
+            #define FIRO_FALLTHROUGH [[gnu::fallthrough]]
+        #endif
+    #elif defined(__has_attribute) && __has_attribute(fallthrough)
+        #define FIRO_FALLTHROUGH __attribute__((fallthrough))
+    #elif defined(__GNUC__) && (__GNUC__ >= 7) && !defined(__clang__)
+        #define FIRO_FALLTHROUGH __attribute__((fallthrough))
+    #endif
 #else
   #define FIRO_FALLTHROUGH /* fallthrough */
 #endif
