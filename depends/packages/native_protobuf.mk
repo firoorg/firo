a341151c87324a54463ee590cf4ffc7bc51df8a1
AlefLm <aleflm@proton.me>
2025-05-15 06:20:29 +0000

    update contrib protobuf version

--- depends/packages/native_protobuf.mk
+++ depends/packages/native_protobuf.mk
@@ -1,8 +1,8 @@
 package=native_protobuf
-$(package)_version=2.6.1
+$(package)_version=30.2
 $(package)_download_path=https://github.com/google/protobuf/releases/download/v$($(package)_version)
-$(package)_file_name=protobuf-$($(package)_version).tar.bz2
-$(package)_sha256_hash=ee445612d544d885ae240ffbcbf9267faa9f593b7b101f21d58beceb92661910
+$(package)_file_name=protobuf-$($(package)_version).tar.gz
+$(package)_sha256_hash=fb06709acc393cc36f87c251bb28a5500a2e12936d4346099f2c6240f6c7a941
 
 define $(package)_set_vars
 $(package)_config_opts=--disable-shared --without-zlib
