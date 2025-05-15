e4e401a589435a01c67cbaf6ac614221780af132
AlefLm <aleflm@proton.me>
2025-05-15 06:20:45 +0000

    update contrib zlib version

--- depends/packages/zlib.mk
+++ depends/packages/zlib.mk
@@ -1,8 +1,8 @@
 package=zlib
-$(package)_version=1.2.11
+$(package)_version=1.3.1
 $(package)_download_path=http://www.zlib.net
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1
+$(package)_sha256_hash=9a93b2b7dfdac77ceba5a558a580e74667dd6fede4585b91eefb60f03b72df23
 
 define $(package)_set_vars
 $(package)_build_opts= CC="$($(package)_cc)"
