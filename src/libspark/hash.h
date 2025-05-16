c631a5e497f903e2aa424cb7c780ccf7a64663bf
Gevorg Voskanyan <gevbsd@gmail.com>
2025-05-16 14:17:06 +0400

    clang-format a few libspark/ files

--- src/libspark/hash.h
+++ src/libspark/hash.h
@@ -13,19 +13,19 @@ using namespace secp_primitives;
 
 class Hash {
 public:
-    explicit Hash(const std::string& label);
-    ~Hash();
-    void include(const CDataStream& data);
-    void include(std::span<const unsigned char> data);
-    std::vector<unsigned char> finalize();
-    Scalar finalize_scalar();
-    GroupElement finalize_group();
+   explicit Hash( const std::string &label );
+   ~Hash();
+   void include( const CDataStream &data );
+   void include( std::span< const unsigned char > data );
+   std::vector< unsigned char > finalize();
+   Scalar finalize_scalar();
+   GroupElement finalize_group();
 
 private:
-    void include_size(std::size_t size);
-    EVP_MD_CTX* ctx;
+   void include_size( std::size_t size );
+   EVP_MD_CTX *ctx;
 };
 
-} // namespace spark
+}   // namespace spark
 
 #endif
