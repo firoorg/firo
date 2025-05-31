880c3f2d64f7aeed9ee07d80bb59f1acb2749488
AlefLM <aleflm@proton.me>
2025-06-01 00:13:45 +0330

    Remove native_clang package and dependency from native_libtapi and packages.mk

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -23,9 +23,5 @@ $(host_arch)_$(host_os)_native_packages += native_b2
 ifneq ($(build_os),darwin)
 darwin_native_packages += native_cctools native_libtapi native_libdmg-hfsplus
 
-ifeq ($(strip $(FORCE_USE_SYSTEM_CLANG)),)
-darwin_native_packages+= native_clang
-endif
-
 endif
 
