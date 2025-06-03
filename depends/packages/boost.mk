6ef9912fbc1e80c6ad3b4c490426c540bff6d0fe
AlefLm <aleflm@proton.me>
2025-06-03 17:32:03 +0100

    fixed boost build

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -20,7 +20,6 @@ $(package)_config_opts_i686_linux=address-model=32 architecture=x86
 $(package)_toolset_$(host_os)=gcc
 $(package)_archiver_$(host_os)=$($(package)_ar)
 $(package)_toolset_darwin=darwin
-$(package)_archiver_darwin=$($(package)_libtool)
 $(package)_cxxflags=-std=c++17
 $(package)_cxxflags_linux+=-fPIC
 $(package)_cxxflags_freebsd+=-fPIC
@@ -28,17 +27,25 @@ $(package)_cxxflags_darwin+=-ffile-prefix-map=$($(package)_extract_dir)=/usr
 endef
 
 define $(package)_preprocess_cmds
-  echo "using $($(package)_toolset_$(host_os)) : : $($(package)_cxx) : <cflags>\"$($(package)_cflags)\" <cxxflags>\"$($(package)_cxxflags)\" <compileflags>\"$($(package)_cppflags)\" <linkflags>\"$($(package)_ldflags)\" <archiver>\"$(boost_archiver_$(host_os))\" <striper>\"$(host_STRIP)\"  <ranlib>\"$(host_RANLIB)\" <rc>\"$(host_WINDRES)\" : ;" > user-config.jam && cat user-config.jam
+  echo "using $($(package)_toolset_$(host_os)) : : $($(package)_cxx) : <cflags>\"$($(package)_cflags)\" <cxxflags>\"$($(package)_cxxflags)\" <compileflags>\"$($(package)_cppflags)\" <linkflags>\"$($(package)_ldflags)\" <archiver>\"$($(package)_archiver_$(host_os))\" <striper>\"$(host_STRIP)\"  <ranlib>\"$(host_RANLIB)\" <rc>\"$(host_WINDRES)\" : ;" > user-config.jam && cat user-config.jam
 endef
 
+# Detect if GUIX_ENVIRONMENT is set AND it is compiling to apple then patch with fix_boost_jam_cross-compilation.patch
 define $(package)_config_cmds
-  ./bootstrap.sh --without-icu --with-toolset=$($(package)_toolset_$(host_os)) --with-bjam=b2
+  ./bootstrap.sh --without-icu --with-toolset=$($(package)_toolset_$(host_os)) --with-bjam=b2 && \
+  if [ -n "$$GUIX_ENVIRONMENT" ] && [ "$(host_os)" = "darwin" ]; then \
+    echo "GUIX_ENVIRONMENT detected and building for Darwin - applying patch"; \
+    echo "Patching with fix_boost_jam_cross-compilation.patch"; \
+    patch -p1 < $($(package)_patch_dir)/fix_boost_jam_cross-compilation.patch; \
+  else \
+    echo "Skipping patch: GUIX_ENVIRONMENT=$$GUIX_ENVIRONMENT, host_os=$(host_os)"; \
+  fi
 endef
 
 define $(package)_build_cmds
-  b2 -d2 -j2 -d1 --prefix=$($(package)_staging_prefix_dir) $($(package)_config_opts) toolset=$($(package)_toolset_$(host_os)) --with-atomic --with-chrono --with-filesystem --with-program_options --with-system --with-thread --with-test stage
+  b2 -d2 -j`nproc` -d1 --prefix=$($(package)_staging_prefix_dir) $($(package)_config_opts) toolset=$($(package)_toolset_$(host_os)) --with-atomic --with-chrono --with-filesystem --with-program_options --with-system --with-thread --with-test --no-cmake-config stage
 endef
 
 define $(package)_stage_cmds
-  b2 -d0 -j4 --prefix=$($(package)_staging_prefix_dir) $($(package)_config_opts) toolset=$($(package)_toolset_$(host_os)) --with-atomic --with-chrono --with-filesystem --with-program_options --with-system --with-thread --with-test --no-cmake-config install
+  b2 -d2 -j`nproc` --prefix=$($(package)_staging_prefix_dir) $($(package)_config_opts) toolset=$($(package)_toolset_$(host_os)) --with-atomic --with-chrono --with-filesystem --with-program_options --with-system --with-thread --with-test --no-cmake-config install
 endef
