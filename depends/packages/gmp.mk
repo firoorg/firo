a65b8eb8f8183630b4a10d9a2f7d135ea478a3d2
AlefLM <aleflm@proton.me>
2025-06-05 01:54:02 +0330

    Add linker and sdk path to build.sh for guix Retouch CI flow Add endian definitions for macOS in endian.h Unset environment variables to prevent conflict between glibc and SDK header files Disable assembly for guix for x86_64-apple-darwin (linker cannot link .o with mainproject) Fix hash of SDK, some minor changes, skip other jobs except two apple job

--- depends/packages/gmp.mk
+++ depends/packages/gmp.mk
@@ -6,16 +6,25 @@ $(package)_sha256_hash=eae9326beb4158c386e39a356818031bd28f3124cf915f8c5b1dc4c7a
 $(package)_patches=applem1.patch
 
 define $(package)_set_vars
-$(package)_config_opts+=--enable-cxx --enable-fat --with-pic --disable-shared
+$(package)_config_opts+=--enable-cxx --with-pic --disable-shared
+ifndef GUIX_ENVIRONMENT
+  $(package)_config_opts += --enable-fat
+else
+  ifneq ($(HOST),x86_64-apple-darwin)
+    $(package)_config_opts += --enable-fat
+  endif
+endif
 $(package)_config_opts_arm64_darwin+=--build=$(BUILD) --host=$(subst arm64,aarch64,$(HOST))
 $(package)_cflags_armv7l_linux+=-march=armv7-a
 $(package)_config_opts_mingw32+=CC_FOR_BUILD=gcc gmp_cv_prog_exeext_for_build=
 endef
 
 # Guix tries to use lld as the linker, but it does not support arm64-darwin
+# --disable-assembly needs to disable fat
 ifdef GUIX_ENVIRONMENT
 $(package)_cflags_darwin+=-fuse-ld=lld
 $(package)_cxxflags_darwin+=-fuse-ld=lld
+$(package)_config_opts_x86_64_darwin+=--disable-assembly
 endif
 
 define $(package)_config_cmds
