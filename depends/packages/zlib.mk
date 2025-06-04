6f258230de8d9f6b6091f024e7c4dd4d8bd0e587
AlefLm <aleflm@proton.me>
2025-06-04 09:52:09 +0000

    fix zlib with cmake

--- depends/packages/zlib.mk
+++ depends/packages/zlib.mk
@@ -5,21 +5,20 @@ $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_sha256_hash=9a93b2b7dfdac77ceba5a558a580e74667dd6fede4585b91eefb60f03b72df23
 
 define $(package)_set_vars
-$(package)_build_opts= CC="$($(package)_cc)"
-$(package)_build_opts+=CFLAGS="$($(package)_cflags) $($(package)_cppflags) -fPIC"
-$(package)_build_opts+=RANLIB="$($(package)_ranlib)"
-$(package)_build_opts+=AR="$($(package)_ar)"
+$(package)_cflags=-fPIC
+$(package)_cxxflags=-fPIC
 endef
 
 define $(package)_config_cmds
-  ./configure --static --prefix=$(host_prefix)
+  echo "Configuring $($(package)_file_name) for $($(package)_staging_dir)" && \
+  cmake -S . -B . -DZLIB_BUILD_TESTING=OFF -DCMAKE_PREFIX_PATH=$($(package)_staging_dir)
 endef
 
 define $(package)_build_cmds
-  $(MAKE) $($(package)_build_opts) libz.a
+  $(MAKE)
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install $($(package)_build_opts)
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install
 endef
 
