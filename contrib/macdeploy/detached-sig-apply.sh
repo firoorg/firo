ece7ab69d324bd772e7c86dec72046bc9b6037aa
AlefLm <AlefLm@proton.me>
2025-07-20 23:51:37 +0330

    Update macdeploy to notarize too

--- contrib/macdeploy/detached-sig-apply.sh
+++ contrib/macdeploy/detached-sig-apply.sh
@@ -8,20 +8,29 @@ set -e
 
 UNSIGNED="$1"
 SIGNED="$2"
+APPLE_API_KEY="$3"
+APPLE_ISSUER_ID="$4"
 ROOTDIR=dist
 OUTDIR=signed-app
 SIGNAPPLE=signapple
 
-if [ -z "$UNSIGNED" ]; then
-  echo "usage: $0 <unsigned app> <signed app>"
-  exit 1
-fi
-
-if [ -z "$SIGNED" ]; then
-  echo "usage: $0 <unsigned app> <signed app>"
+if [ -z "$UNSIGNED" ] || [ -z "$SIGNED" ]; then
+  echo "usage: $0 <unsigned app> <signed app> [apple_api_key] [apple_issuer_id]"
   exit 1
 fi
 
 ${SIGNAPPLE} apply ${UNSIGNED} ${SIGNED}
+
+# Notarize if Apple API key and Issuer ID are provided
+if [ -n "$APPLE_API_KEY" ] && [ -n "$APPLE_ISSUER_ID" ]; then
+  stty -echo
+  printf "Enter the passphrase for %s: " ${APPLE_API_KEY}
+  read api_key_pass
+  printf "\n"
+  stty echo
+  ${SIGNAPPLE} notarize --passphrase "$api_key_pass" ${APPLE_API_KEY} ${APPLE_ISSUER_ID} ${UNSIGNED}
+  echo "Notarization requested."
+fi
+
 mv ${ROOTDIR} ${OUTDIR}
 echo "Signed: ${OUTDIR}"
