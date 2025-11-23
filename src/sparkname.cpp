9a38a668df53873bc85e808a59600397de6b3337
Peter Shugalev <peter@shugalev.com>
2025-11-23 22:22:11 +0100

    Spark Names V2.1: tweak expiration rules for renewal and transfer

--- src/sparkname.cpp
+++ src/sparkname.cpp
@@ -174,11 +174,35 @@ bool CSparkNameManager::CheckSparkNameTx(const CTransaction &tx, int nHeight, CV
     if (!IsSparkNameValid(sparkNameData.name))
         return state.DoS(100, error("CheckSparkNameTx: invalid name"));
 
-    constexpr int nBlockPerYear = 365*24*24; // 24 blocks per hour
-    int nYears = (sparkNameData.sparkNameValidityBlocks + nBlockPerYear-1) / nBlockPerYear;
+    int existingExpirationHeight = -1;
+    bool fUpdateExistingRecord = false;
+    bool fSparkNameTransfer = sparkNameData.nVersion >= 2 && sparkNameData.operationType == (uint8_t)CSparkNameTxData::opTransfer;
 
-    if (sparkNameData.sparkNameValidityBlocks > nBlockPerYear * 10)
-        return state.DoS(100, error("CheckSparkNameTx: can't be valid for more than 10 years"));
+    if (sparkNames.count(ToUpper(sparkNameData.name)) > 0) {
+        // it's possible to change any metadata of the existing name but if the spark address is being
+        // tranferred, new name shouldn't be already registered
+        if (!fSparkNameTransfer && sparkNames[ToUpper(sparkNameData.name)].sparkAddress != sparkNameData.sparkAddress)
+            return state.DoS(100, error("CheckSparkNameTx: name already exists"));
+
+        fUpdateExistingRecord = true;
+        existingExpirationHeight = sparkNames[ToUpper(sparkNameData.name)].sparkNameValidityHeight;
+    }
+
+    constexpr int nBlockPerYear = 365*24*24; // 24 blocks per hour
+    int validityBlocks = sparkNameData.sparkNameValidityBlocks;
+    if (nHeight >= consensusParams.nSparkNamesV21StartBlock) {
+        if (existingExpirationHeight != -1)
+            validityBlocks = std::max(validityBlocks, existingExpirationHeight - nHeight + validityBlocks);
+        // after nSparkNamesV21StartBlock, max validity is 15 years
+        if (validityBlocks > nBlockPerYear * 15)
+            return state.DoS(100, error("CheckSparkNameTx: can't be valid for more than 15 years"));
+    }
+    else {
+        if (validityBlocks > nBlockPerYear * 10)
+            return state.DoS(100, error("CheckSparkNameTx: can't be valid for more than 10 years"));
+    }
+
+    int nYears = (validityBlocks + nBlockPerYear-1) / nBlockPerYear;
 
     CAmount nameFee = consensusParams.nSparkNamesFee[sparkNameData.name.size()] * COIN * nYears;
 
@@ -195,18 +219,6 @@ bool CSparkNameManager::CheckSparkNameTx(const CTransaction &tx, int nHeight, CV
 
     if (sparkNameData.additionalInfo.size() > 1024)
         return state.DoS(100, error("CheckSparkNameTx: additional info is too long"));
-
-    bool fUpdateExistingRecord = false;
-    bool fSparkNameTransfer = sparkNameData.nVersion >= 2 && sparkNameData.operationType == (uint8_t)CSparkNameTxData::opTransfer;
-
-    if (sparkNames.count(ToUpper(sparkNameData.name)) > 0) {
-        // it's possible to change any metadata of the existing name but if the spark address is being
-        // tranferred, new name shouldn't be already registered
-        if (!fSparkNameTransfer && sparkNames[ToUpper(sparkNameData.name)].sparkAddress != sparkNameData.sparkAddress)
-            return state.DoS(100, error("CheckSparkNameTx: name already exists"));
-
-        fUpdateExistingRecord = true;
-    }
 
     {
         LOCK(cs_spark_name);
