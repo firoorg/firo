8a821fcd8c153c17feca967e80e4f053717a18c4
aleflm <164785527+aleflm@users.noreply.github.com>
2025-12-15 13:19:57 +0330

    Enable C++20 support needed for Bitcoin core libraries. (#1719)
    Update Boost 1.83.0 since 1.81.0 does not work with cpp20.

--- depends/packages/zeromq.mk
+++ depends/packages/zeromq.mk
@@ -12,7 +12,7 @@ define $(package)_set_vars
   $(package)_config_opts += --disable-Werror --disable-drafts --enable-option-checking
   $(package)_config_opts_linux=--with-pic
   $(package)_config_opts_android=--with-pic
-  $(package)_cxxflags=-std=c++17
+  $(package)_cxxflags=-std=c++20
 ifdef GUIX_ENVIRONMENT
 $(package)_config_env_x86_64_darwin = LIB_LIBRARY_BACKUP="$(LIBRARY_PATH)" LIBRARY_PATH=""
 endif
