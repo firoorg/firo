firoorg/firo

commit 9e9b18a5316f57079fac328e44e720571d7abfa3

Added wayland support for qt

depends/packages/qt.mk
@@ -7,7 +7,7 @@ $(package)_sha256_hash=$(qt_details_qtbase_sha256_hash)
 ifneq ($(host),$(build))
 $(package)_dependencies := native_$(package)
 endif
-$(package)_linux_dependencies := freetype fontconfig libxcb libxkbcommon libxcb_util libxcb_util_cursor libxcb_util_render libxcb_util_keysyms libxcb_util_image libxcb_util_wm
+$(package)_linux_dependencies := freetype fontconfig libxcb libxkbcommon libxcb_util libxcb_util_cursor libxcb_util_render libxcb_util_keysyms libxcb_util_image libxcb_util_wm wayland libglvnd
 $(package)_freebsd_dependencies := $($(package)_linux_dependencies)
 $(package)_patches_path := $(qt_details_patches_path)
 $(package)_patches := dont_hardcode_pwd.patch
@@ -26,8 +26,12 @@ $(package)_qttranslations_sha256_hash=$(qt_details_qttranslations_sha256_hash)
 $(package)_qttools_file_name=$(qt_details_qttools_file_name)
 $(package)_qttools_sha256_hash=$(qt_details_qttools_sha256_hash)
 
+$(package)_qtwayland_file_name = qtwayland-$($(package)_suffix)
+$(package)_qtwayland_sha256_hash = 686ada6c5525bfa7f39aa37e2a811a381374c2c4137a7d9883a19ff24209240b
+
 $(package)_extra_sources := $($(package)_qttranslations_file_name)
 $(package)_extra_sources += $($(package)_qttools_file_name)
+$(package)_extra_sources += $($(package)_qtwayland_file_name)
 
 $(package)_top_download_path=$(qt_details_top_download_path)
 $(package)_top_cmakelists_file_name=$(qt_details_top_cmakelists_file_name)
@@ -49,13 +53,11 @@ define $(package)_set_vars
 $(package)_config_env = QT_MAC_SDK_NO_VERSION_CHECK=1
 $(package)_config_opts_release = -release
 $(package)_config_opts_debug = -debug
-$(package)_config_opts = -no-egl
 $(package)_config_opts_debug += -optimized-tools
 $(package)_config_opts += -bindir $(build_prefix)/bin
 $(package)_config_opts += -c++std c++20
 $(package)_config_opts += -confirm-license
 $(package)_config_opts += -no-cups
-$(package)_config_opts += -no-egl
 $(package)_config_opts += -no-eglfs
 $(package)_config_opts += -no-evdev
 $(package)_config_opts += -no-gif
@@ -69,7 +71,6 @@ $(package)_config_opts += -no-libjpeg
 $(package)_config_opts += -no-libproxy
 $(package)_config_opts += -no-libudev
 $(package)_config_opts += -no-mtdev
-$(package)_config_opts += -no-opengl
 $(package)_config_opts += -no-openssl
 $(package)_config_opts += -no-openvg
 $(package)_config_opts += -no-reduce-relocations
@@ -175,10 +176,23 @@ $(package)_config_opts_linux += -no-xcb-xlib
 $(package)_config_opts_linux += -pkg-config
 $(package)_config_opts_linux += -system-freetype
 $(package)_config_opts_linux += -fontconfig
-$(package)_config_opts_linux += -no-opengl
 $(package)_config_opts_linux += -no-feature-vulkan
 $(package)_config_opts_linux += -dbus-runtime
 $(package)_config_opts_linux += -feature-xcb
+
+# Wayland-specific options:
+$(package)_config_opts_linux += -egl
+$(package)_config_opts_linux += -opengl es2
+$(package)_config_opts_linux += -no-opengles3
+$(package)_config_opts_linux += -no-feature-wayland-drm-egl-server-buffer
+$(package)_config_opts_linux += -no-feature-wayland-shm-emulation-server-buffer
+$(package)_config_opts_linux += -no-feature-wayland-client-fullscreen-shell-v1
+$(package)_config_opts_linux += -no-feature-wayland-client-ivi-shell
+$(package)_config_opts_linux += -no-feature-wayland-client-wl-shell
+$(package)_config_opts_linux += -no-feature-wayland-client-xdg-shell-v5
+$(package)_config_opts_linux += -no-feature-wayland-client-xdg-shell-v6
+$(package)_config_opts_linux += -no-feature-wayland-server
+
 ifneq ($(LTO),)
 $(package)_config_opts_linux += -ltcg
 endif
@@ -277,6 +291,7 @@ define $(package)_fetch_cmds
 $(call fetch_file,$(package),$($(package)_download_path),$($(package)_download_file),$($(package)_file_name),$($(package)_sha256_hash)) && \
 $(call fetch_file,$(package),$($(package)_download_path),$($(package)_qttranslations_file_name),$($(package)_qttranslations_file_name),$($(package)_qttranslations_sha256_hash)) && \
 $(call fetch_file,$(package),$($(package)_download_path),$($(package)_qttools_file_name),$($(package)_qttools_file_name),$($(package)_qttools_sha256_hash)) && \
+$(call fetch_file,$(package),$($(package)_download_path),$($(package)_qtwayland_file_name),$($(package)_qtwayland_file_name),$($(package)_qtwayland_sha256_hash)) && \
 $(call fetch_file,$(package),$($(package)_top_download_path),$($(package)_top_cmakelists_download_file),$($(package)_top_cmakelists_file_name)-$($(package)_version),$($(package)_top_cmakelists_sha256_hash)) && \
 $(call fetch_file,$(package),$($(package)_top_cmake_download_path),$($(package)_top_cmake_ecmoptionaladdsubdirectory_download_file),$($(package)_top_cmake_ecmoptionaladdsubdirectory_file_name)-$($(package)_version),$($(package)_top_cmake_ecmoptionaladdsubdirectory_sha256_hash)) && \
 $(call fetch_file,$(package),$($(package)_top_cmake_download_path),$($(package)_top_cmake_qttoplevelhelpers_download_file),$($(package)_top_cmake_qttoplevelhelpers_file_name)-$($(package)_version),$($(package)_top_cmake_qttoplevelhelpers_sha256_hash))
@@ -288,6 +303,7 @@ define $(package)_extract_cmds
   echo "$($(package)_sha256_hash)  $($(package)_source)" > $($(package)_extract_dir)/.$($(package)_file_name).hash && \
   echo "$($(package)_qttranslations_sha256_hash)  $($(package)_source_dir)/$($(package)_qttranslations_file_name)" >> $($(package)_extract_dir)/.$($(package)_file_name).hash && \
   echo "$($(package)_qttools_sha256_hash)  $($(package)_source_dir)/$($(package)_qttools_file_name)" >> $($(package)_extract_dir)/.$($(package)_file_name).hash && \
+  echo "$($(package)_qtwayland_sha256_hash)  $($(package)_source_dir)/$($(package)_qtwayland_file_name)" >> $($(package)_extract_dir)/.$($(package)_file_name).hash && \
   echo "$($(package)_top_cmakelists_sha256_hash)  $($(package)_source_dir)/$($(package)_top_cmakelists_file_name)-$($(package)_version)" >> $($(package)_extract_dir)/.$($(package)_file_name).hash && \
   echo "$($(package)_top_cmake_ecmoptionaladdsubdirectory_sha256_hash)  $($(package)_source_dir)/$($(package)_top_cmake_ecmoptionaladdsubdirectory_file_name)-$($(package)_version)" >> $($(package)_extract_dir)/.$($(package)_file_name).hash && \
   echo "$($(package)_top_cmake_qttoplevelhelpers_sha256_hash)  $($(package)_source_dir)/$($(package)_top_cmake_qttoplevelhelpers_file_name)-$($(package)_version)" >> $($(package)_extract_dir)/.$($(package)_file_name).hash && \
@@ -298,6 +314,9 @@ define $(package)_extract_cmds
   $(build_TAR) --no-same-owner --strip-components=1 -xf $($(package)_source_dir)/$($(package)_qttranslations_file_name) -C qttranslations && \
   mkdir qttools && \
   $(build_TAR) --no-same-owner --strip-components=1 -xf $($(package)_source_dir)/$($(package)_qttools_file_name) -C qttools && \
+  $(build_TAR) --no-same-owner --strip-components=1 -xf $($(package)_source_dir)/$($(package)_qttools_file_name) -C qttools && \
+  mkdir qtwayland && \
+  $(build_TAR) --no-same-owner --strip-components=1 -xf $($(package)_source_dir)/$($(package)_qtwayland_file_name) -C qtwayland && \
   cp $($(package)_source_dir)/$($(package)_top_cmakelists_file_name)-$($(package)_version) ./$($(package)_top_cmakelists_file_name) && \
   mkdir cmake && \
   cp $($(package)_source_dir)/$($(package)_top_cmake_ecmoptionaladdsubdirectory_file_name)-$($(package)_version) cmake/$($(package)_top_cmake_ecmoptionaladdsubdirectory_file_name) && \
@@ -351,6 +370,10 @@ endef
 define $(package)_stage_cmds
   cmake --install . --prefix $($(package)_staging_prefix_dir)
 endef
+
+ifeq ($(host_os),linux)
+  $(package)_stage_cmds += && $(MAKE) -C qtwayland/src INSTALL_ROOT=$($(package)_staging_dir) sub-plugins-install_subtargets
+endif
 
 define $(package)_postprocess_cmds
   rm -rf doc/

depends/packages/wayland.mk
@@ -1,0 +1,29 @@
+package := wayland
+native_package := native_wayland_scanner
+$(package)_version = $($(native_package)_version)
+$(package)_download_path = $($(native_package)_download_path)
+$(package)_file_name = $($(native_package)_file_name)
+$(package)_sha256_hash = $($(native_package)_sha256_hash)
+$(package)_dependencies = $(native_package) libffi expat
+
+define $(package)_set_vars
+  $(package)_config_opts := --enable-option-checking --disable-dependency-tracking
+  $(package)_config_opts += --enable-shared --disable-static --disable-documentation
+  $(package)_config_opts += --disable-dtd-validation --with-host-scanner
+endef
+
+define $(package)_config_cmds
+  $($(package)_autoconf)
+endef
+
+define $(package)_build_cmds
+  $(MAKE)
+endef
+
+define $(package)_stage_cmds
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+endef
+
+define $(package)_postprocess_cmds
+  rm lib/*.la
+endef

contrib/guix/manifest.scm
@@ -21,6 +21,7 @@
              ((gnu packages python-crypto) #:select (python-asn1crypto))
              ((gnu packages tls) #:select (openssl))
              (gnu packages perl)
+             (gnu packages xml)
              ((gnu packages version-control) #:select (git-minimal))
              (guix build-system cmake)
              (guix build-system gnu)

depends/Makefile
@@ -159,7 +159,7 @@ libevent_packages_$(NO_LIBEVENT) = $(libevent_packages)
 qrencode_packages_$(NO_QR) = $(qrencode_packages)
 
 qt_packages_$(NO_QT) = $(qt_packages) $(qt_$(host_os)_packages) $(qt_$(host_arch)_$(host_os)_packages) $(qrencode_packages_)
-qt_native_packages_$(NO_QT) = $(qt_native_packages)
+qt_native_packages_$(NO_QT) = $(qt_native_packages) $(qt_$(host_os)_native_packages) $(qt_$(host_arch)_$(host_os)_native_packages)
 
 bdb_packages_$(NO_BDB) = $(bdb_packages)
 sqlite_packages_$(NO_SQLITE) = $(sqlite_packages)

depends/packages/libffi.mk
@@ -1,0 +1,27 @@
+package := libffi
+$(package)_version := 3.5.2
+$(package)_download_path := https://github.com/libffi/$(package)/releases/download/v$($(package)_version)
+$(package)_file_name := $(package)-$($(package)_version).tar.gz
+$(package)_sha256_hash := f3a3082a23b37c293a4fcd1053147b371f2ff91fa7ea1b2a52e335676bac82dc
+
+define $(package)_set_vars
+  $(package)_config_opts := --enable-option-checking --disable-dependency-tracking
+  $(package)_config_opts += --enable-shared --disable-static --disable-docs
+  $(package)_config_opts += --disable-multi-os-directory
+endef
+
+define $(package)_config_cmds
+  $($(package)_autoconf)
+endef
+
+define $(package)_build_cmds
+  $(MAKE)
+endef
+
+define $(package)_stage_cmds
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+endef
+
+define $(package)_postprocess_cmds
+  rm -rf share
+endef

depends/packages/libglvnd.mk
@@ -1,0 +1,28 @@
+package := libglvnd
+$(package)_version := 1.4.0
+$(package)_download_path := https://gitlab.freedesktop.org/glvnd/$(package)/-/archive/v$($(package)_version)
+$(package)_file_name := $(package)-v$($(package)_version).tar.bz2
+$(package)_sha256_hash := fdf395391d95f270528dbff6ce2ee54c186753d286ad62e0da5f62c6f67ba915
+
+define $(package)_set_vars
+  $(package)_config_opts := --enable-option-checking --disable-dependency-tracking
+  $(package)_config_opts += --enable-shared --disable-static
+  $(package)_config_opts += --disable-x11 --disable-gles1
+endef
+
+define $(package)_config_cmds
+  ./autogen.sh && \
+  $($(package)_autoconf)
+endef
+
+define $(package)_build_cmds
+  $(MAKE)
+endef
+
+define $(package)_stage_cmds
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+endef
+
+define $(package)_postprocess_cmds
+  rm lib/*.la
+endef

depends/packages/native_wayland_scanner.mk
@@ -1,0 +1,25 @@
+package := native_wayland_scanner
+$(package)_version := 1.19.0
+$(package)_download_path := https://wayland.freedesktop.org/releases
+$(package)_file_name := wayland-$($(package)_version).tar.xz
+$(package)_sha256_hash := baccd902300d354581cd5ad3cc49daa4921d55fb416a5883e218750fef166d15
+
+define $(package)_set_vars
+  $(package)_config_opts := --enable-option-checking --disable-dependency-tracking
+  $(package)_config_opts += --disable-libraries --disable-documentation
+  $(package)_config_opts += --disable-dtd-validation
+endef
+
+define $(package)_config_cmds
+  EXPAT_LIBS=$$$$(env -u PKG_CONFIG_LIBDIR PKG_CONFIG_PATH=$(SYSTEM_PKG_CONFIG_PATH) pkg-config --libs expat) \
+  EXPAT_CFLAGS=$$$$(env -u PKG_CONFIG_LIBDIR PKG_CONFIG_PATH=$(SYSTEM_PKG_CONFIG_PATH) PKG_CONFIG_ALLOW_SYSTEM_CFLAGS=1 pkg-config --cflags expat) \
+  $($(package)_autoconf)
+endef
+
+define $(package)_build_cmds
+  $(MAKE)
+endef
+
+define $(package)_stage_cmds
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+endef

depends/packages/packages.mk
@@ -3,7 +3,8 @@ darwin_packages:=zeromq
 linux_packages:=zeromq
 native_packages :=
 
-qt_linux_packages:=qt expat libxcb xcb_proto libXau xproto freetype fontconfig libxkbcommon libxcb_util libxcb_util_cursor libxcb_util_render libxcb_util_keysyms libxcb_util_image libxcb_util_wm
+qt_linux_packages:=qt expat libxcb xcb_proto libXau xproto freetype fontconfig libxkbcommon libxcb_util libxcb_util_cursor libxcb_util_render libxcb_util_keysyms libxcb_util_image libxcb_util_wm libffi wayland libglvnd
+qt_linux_native_packages := native_wayland_scanner
 qt_freebsd_packages:=$(qt_linux_packages)
 qt_darwin_packages=qt
 qt_mingw32_packages=qt

commit 19a1caaec5a51e352fe5d4ebba869693be5e78a0

Fix wayland support build failures
Three bugs were introduced in commit 9e9b18a (Added wayland support for qt):

1. qtwayland file name used undefined variable $(qt_suffix) instead of
   $(qt_details_suffix), causing the filename to resolve to 'qtwayland-'
   (empty suffix). This caused 404 download errors on all Guix builds and
   hash verification failures on Mac cmake builds.

2. Duplicate qttools extraction line in the host==build extract_cmds
   section extracted the same tarball twice.

3. Wayland configure failed with 'CLOCK_MONOTONIC is needed to compile
   wayland libraries' because the depends system uses -std=c11 (strict C
   standard mode) which hides POSIX symbols like clock_gettime and
   CLOCK_MONOTONIC from <time.h>. Adding -D_GNU_SOURCE to wayland's
   CPPFLAGS makes these symbols visible.

Co-authored-by: Reuben Yap <reuben@firo.org>

depends/packages/qt.mk
@@ -26,7 +26,7 @@ $(package)_qttranslations_sha256_hash=$(qt_details_qttranslations_sha256_hash)
 $(package)_qttools_file_name=$(qt_details_qttools_file_name)
 $(package)_qttools_sha256_hash=$(qt_details_qttools_sha256_hash)
 
-$(package)_qtwayland_file_name = qtwayland-$($(package)_suffix)
+$(package)_qtwayland_file_name = qtwayland-$(qt_details_suffix)
 $(package)_qtwayland_sha256_hash = 686ada6c5525bfa7f39aa37e2a811a381374c2c4137a7d9883a19ff24209240b
 
 $(package)_extra_sources := $($(package)_qttranslations_file_name)
@@ -313,7 +313,6 @@ define $(package)_extract_cmds
   mkdir qttranslations && \
   $(build_TAR) --no-same-owner --strip-components=1 -xf $($(package)_source_dir)/$($(package)_qttranslations_file_name) -C qttranslations && \
   mkdir qttools && \
-  $(build_TAR) --no-same-owner --strip-components=1 -xf $($(package)_source_dir)/$($(package)_qttools_file_name) -C qttools && \
   $(build_TAR) --no-same-owner --strip-components=1 -xf $($(package)_source_dir)/$($(package)_qttools_file_name) -C qttools && \
   mkdir qtwayland && \
   $(build_TAR) --no-same-owner --strip-components=1 -xf $($(package)_source_dir)/$($(package)_qtwayland_file_name) -C qtwayland && \

depends/packages/wayland.mk
@@ -10,6 +10,7 @@ define $(package)_set_vars
   $(package)_config_opts := --enable-option-checking --disable-dependency-tracking
   $(package)_config_opts += --enable-shared --disable-static --disable-documentation
   $(package)_config_opts += --disable-dtd-validation --with-host-scanner
+  $(package)_cppflags += -D_GNU_SOURCE
 endef
 
 define $(package)_config_cmds