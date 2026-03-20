f5db941e04b275814ce9a42311789568b9e4b390
Peter Shugalev <peter@shugalev.com>
2026-03-20 13:36:14 +0100

    Fixed wayland compilation

--- depends/packages/native_wayland_scanner.mk
+++ depends/packages/native_wayland_scanner.mk
@@ -1,26 +1,28 @@
 package := native_wayland_scanner
-$(package)_version := 1.19.0
+$(package)_version := 1.20.0
 $(package)_download_path := https://wayland.freedesktop.org/releases
 $(package)_file_name := wayland-$($(package)_version).tar.xz
-$(package)_sha256_hash := baccd902300d354581cd5ad3cc49daa4921d55fb416a5883e218750fef166d15
+$(package)_sha256_hash := b8a034154c7059772e0fdbd27dbfcda6c732df29cae56a82274f6ec5d7cd8725
 $(package)_dependencies := libffi
 
-define $(package)_set_vars
-  $(package)_config_opts := --enable-option-checking --disable-dependency-tracking
-  $(package)_config_opts += --disable-libraries --disable-documentation
-  $(package)_config_opts += --disable-dtd-validation
-endef
-
 define $(package)_config_cmds
+  PKG_CONFIG_LIBDIR=$(build_prefix)/lib/pkgconfig \
+  PKG_CONFIG_PATH=$(build_prefix)/share/pkgconfig \
   EXPAT_LIBS=$$$$(env -u PKG_CONFIG_LIBDIR PKG_CONFIG_PATH=$(SYSTEM_PKG_CONFIG_PATH) pkg-config --libs expat) \
   EXPAT_CFLAGS=$$$$(env -u PKG_CONFIG_LIBDIR PKG_CONFIG_PATH=$(SYSTEM_PKG_CONFIG_PATH) PKG_CONFIG_ALLOW_SYSTEM_CFLAGS=1 pkg-config --cflags expat) \
-  $($(package)_autoconf)
+  meson setup build --prefix=$(build_prefix) \
+    --libdir=lib \
+    -Dlibraries=false \
+    -Dscanner=true \
+    -Ddocumentation=false \
+    -Dtests=false \
+    -Ddtd_validation=false
 endef
 
 define $(package)_build_cmds
-  $(MAKE)
+  ninja -C build
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+  DESTDIR=$($(package)_staging_dir) ninja -C build install
 endef
--- depends/packages/wayland.mk
+++ depends/packages/wayland.mk
@@ -6,24 +6,30 @@ $(package)_file_name = $($(native_package)_file_name)
 $(package)_sha256_hash = $($(native_package)_sha256_hash)
 $(package)_dependencies = $(native_package) libffi expat
 
-define $(package)_set_vars
-  $(package)_config_opts := --enable-option-checking --disable-dependency-tracking
-  $(package)_config_opts += --enable-shared --disable-static --disable-documentation
-  $(package)_config_opts += --disable-dtd-validation --with-host-scanner
-endef
-
 define $(package)_config_cmds
-  $($(package)_autoconf)
+  PKG_CONFIG_LIBDIR=$(host_prefix)/lib/pkgconfig:$(build_prefix)/lib/pkgconfig \
+  PKG_CONFIG_PATH=$(host_prefix)/share/pkgconfig \
+  CC="$$($(package)_cc)" CXX="$$($(package)_cxx)" \
+  CFLAGS="-D_GNU_SOURCE $$($(package)_cppflags) $$($(package)_cflags)" \
+  CXXFLAGS="$$($(package)_cppflags) $$($(package)_cxxflags)" \
+  LDFLAGS="$$($(package)_ldflags)" \
+  meson setup build --prefix=$(host_prefix) \
+    --libdir=lib \
+    -Dlibraries=true \
+    -Dscanner=false \
+    -Ddocumentation=false \
+    -Dtests=false \
+    -Ddtd_validation=false
 endef
 
 define $(package)_build_cmds
-  $(MAKE)
+  ninja -C build
 endef
 
 define $(package)_stage_cmds
-  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+  DESTDIR=$($(package)_staging_dir) ninja -C build install
 endef
 
 define $(package)_postprocess_cmds
-  rm lib/*.la
+  rm -f lib/*.la
 endef
