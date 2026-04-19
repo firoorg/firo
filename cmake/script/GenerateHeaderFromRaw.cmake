58fc80b787f350f400529a037579e9e412adcc3d
Navid Rahimi <rahimi.nv@gmail.com>
2026-04-19 20:11:32 +0330

    Fix bench_firo macOS C++20 compilation by replacing std::span with std::array and adding missing link dependencies

--- cmake/script/GenerateHeaderFromRaw.cmake
+++ cmake/script/GenerateHeaderFromRaw.cmake
@@ -8,16 +8,17 @@ file(READ ${RAW_SOURCE_PATH} hex_content HEX)
 string(REGEX REPLACE "................" "\\0\n" formatted_bytes "${hex_content}")
 string(REGEX REPLACE "[^\n][^\n]" "std::byte{0x\\0}, " formatted_bytes "${formatted_bytes}")
 
+string(LENGTH "${hex_content}" content_length)
+math(EXPR array_size "${content_length} / 2")
+
 set(header_content
-"#include <cstddef>
-#include <span>
+"#include <array>
+#include <cstddef>
 
 namespace ${RAW_NAMESPACE} {
-inline constexpr std::byte detail_${raw_source_basename}_raw[] {
+inline constexpr std::array<std::byte, ${array_size}> ${raw_source_basename} {{
 ${formatted_bytes}
-};
-
-inline constexpr std::span ${raw_source_basename}{detail_${raw_source_basename}_raw};
+}};
 }
 ")
 file(WRITE ${HEADER_PATH} "${header_content}")
