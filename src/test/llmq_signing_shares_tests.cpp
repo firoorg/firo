c04aaeba59d6699d1ef287b46b96bc2c95f7feb2
Reuben Yap <reuben@firo.org>
2026-08-26 18:34:42 +0800

    Tests: Fix signing shares include order

--- src/test/llmq_signing_shares_tests.cpp
+++ src/test/llmq_signing_shares_tests.cpp
@@ -3,6 +3,7 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#include "llmq/quorums_signing.h"
 #include "llmq/quorums_signing_shares.h"
 #include "test/test_bitcoin.h"
 
