f9e7023b50d93506656fd007e5a8faea5c8672e0
Reuben Yap <reuben@firo.org>
2026-09-22 16:25:18 +0800

    test: exercise DKG mixed-peer signature fallback

--- src/test/llmq_dkgsessionhandler_tests.cpp
+++ src/test/llmq_dkgsessionhandler_tests.cpp
@@ -2,23 +2,74 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "llmq/quorums_dkgsessionhandler.h"
+#include "chainparams.h"
+#include "evo/evodb.h"
+#include "llmq/quorums_dkgsessionmgr.h"
+#include "test/test_bitcoin.h"
+
+#include <array>
 
 #include <boost/test/unit_test.hpp>
 
 namespace llmq
 {
 
-BOOST_AUTO_TEST_SUITE(llmq_dkgsessionhandler_tests)
+template<typename Message>
+std::set<NodeId> BatchVerifyMessageSigs(CDKGSession& session,
+    const std::vector<std::pair<NodeId, std::shared_ptr<Message>>>& messages);
 
-BOOST_AUTO_TEST_CASE(detect_mixed_message_peers)
+struct DKGSessionHandlerTestingSetup : BasicTestingSetup
 {
-    using Message = std::pair<NodeId, std::shared_ptr<int>>;
-    const std::vector<Message> singlePeer{{101, nullptr}, {101, nullptr}};
-    const std::vector<Message> mixedPeers{{101, nullptr}, {101, nullptr}, {202, nullptr}};
+    CBLSWorker blsWorker;
+    CDKGSessionManager dkgManager;
 
-    BOOST_CHECK(detail::BatchNodeIdsAllSame(singlePeer));
-    BOOST_CHECK(!detail::BatchNodeIdsAllSame(mixedPeers));
+    DKGSessionHandlerTestingSetup() :
+        BasicTestingSetup(CBaseChainParams::REGTEST),
+        dkgManager(evoDb->GetRawDB(), blsWorker)
+    {}
+};
+
+BOOST_FIXTURE_TEST_SUITE(llmq_dkgsessionhandler_tests, DKGSessionHandlerTestingSetup)
+
+BOOST_AUTO_TEST_CASE(mixed_peers_fall_back_to_individual_verification)
+{
+    const auto& params = Params().GetConsensus().llmqs.at(Consensus::LLMQ_5_60);
+    std::vector<CBLSSecretKey> keys(3);
+    std::vector<CDeterministicMNCPtr> members;
+    for (size_t i = 0; i < keys.size(); ++i) {
+        keys[i].MakeNewKey();
+        auto state = std::make_shared<CDeterministicMNState>();
+        state->pubKeyOperator.Set(keys[i].GetPublicKey());
+        auto member = std::make_shared<CDeterministicMN>();
+        member->proTxHash = uint256S(strprintf("%064x", i + 1));
+        member->pdmnState = state;
+        members.emplace_back(member);
+    }
+
+    const uint256 quorumHash = uint256S("01");
+    CBlockIndex quorumBlock;
+    quorumBlock.phashBlock = &quorumHash;
+    CDKGSession session(params, blsWorker, dkgManager);
+    BOOST_REQUIRE(session.Init(&quorumBlock, members, uint256()));
+
+    const std::array<NodeId, 3> nodeIds{101, 202, 101};
+    std::vector<std::pair<NodeId, std::shared_ptr<CDKGComplaint>>> messages;
+    for (size_t i = 0; i < members.size(); ++i) {
+        auto message = std::make_shared<CDKGComplaint>(params);
+        message->llmqType = params.type;
+        message->quorumHash = quorumHash;
+        message->proTxHash = members[i]->proTxHash;
+        message->sig = keys[i].Sign(message->GetSignHash());
+        messages.emplace_back(nodeIds[i], std::move(message));
+    }
+
+    CBLSSecretKey wrongKey;
+    wrongKey.MakeNewKey();
+    messages[1].second->sig = wrongKey.Sign(messages[1].second->GetSignHash());
+
+    const auto badNodes = BatchVerifyMessageSigs(session, messages);
+    BOOST_REQUIRE_EQUAL(badNodes.size(), 1U);
+    BOOST_CHECK_EQUAL(*badNodes.begin(), 202);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
